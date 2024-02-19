8be19f7b03ff5fc404afed248702af22bc42dac9
EliaStocco <eliastocco@gmail.com>
2024-02-19 11:47:44 +0100

    added 'enforce_dependency'

--- eslib/nn/network/iPIinterface.py
+++ eslib/nn/network/iPIinterface.py
@@ -4,6 +4,7 @@ from ase.io import read
 from ase.cell import Cell
 from ase import Atoms
 from eslib.nn.dataset import make_datapoint
+from eslib.nn.dataset import enforce_dependency
 from eslib.tools import check_cell_format
 from torch_geometric.data import Data
 from abc import ABC, abstractproperty, abstractmethod
@@ -65,36 +66,68 @@ class iPIinterface(ABC):
         return y
 
     
-    def get(self,pos,cell=None,check=True)->Tuple[torch.tensor,Data]:
+    def get(self,pos=None,cell=None,check=True,X=None)->Tuple[torch.tensor,Data]:
         """Compute the dipole given an the positions and the cell.
         The function requires the atoms to respect the order given by `self._symbols`."""
         # 'cell' has to be in i-PI format (upper triangular) or ase.cell
-        pbc = cell is not None
-        cell = self.correct_cell(cell,check=check)
-        self.eval()
-        requires_grad = {   "pos"        : True,\
-                            "lattice"    : True,\
-                            "x"          : False,\
-                            "edge_vec"   : True,\
-                            "edge_index" : False }
-        X = self.make_datapoint(lattice=cell.T,positions=pos,requires_grad=requires_grad)
-        y = self(X)[0]
+        if X is None:
+            pbc = cell is not None
+            cell = self.correct_cell(cell,check=check)
+            self.eval()
+            requires_grad = {   "pos"        : True,\
+                                "lattice"    : True,\
+                                "x"          : False,\
+                                "edge_vec"   : True,\
+                                "edge_index" : False }
+            X = self.make_datapoint(lattice=cell.T,positions=pos,requires_grad=requires_grad)
+        else:
+            # I need to be sure that `X.edge_vec` depends on `X.pos`.
+            # I just need to recompute `X.edge_vec` using all the infomation that I already have.
+            X = enforce_dependency(X)
+        y = self(X)
+        if y.shape[0] == 1:
+            y = y[0]
         return y, X
     
-    def get_jac(self,pos,cell=None,y=None,X=None)->Tuple[torch.tensor,torch.tensor]:
+    def get_jac(self,pos=None,cell=None,y=None,X=None)->Tuple[torch.tensor,torch.tensor]:
         if y is None or X is None:
             y,X = self.get(pos=pos,cell=cell)
+        n_batch = len(np.unique(X.batch))
+        if n_batch != 1:
+            raise ValueError("'get_jac' works only with one batch.")
         N = len(X.pos.flatten())
         jac = torch.full((N,y.shape[0]),torch.nan)
         for n in range(y.shape[0]):
+            # y[n].backward(retain_graph=True)
             y[n].backward(retain_graph=True)
             jac[:,n] = X.pos.grad.flatten().detach()
             X.pos.grad.data.zero_()
         return jac,X
 
-    def get_value_and_jac(self,pos,cell=None)->Tuple[torch.tensor,torch.tensor,Data]:
-        y,X = self.get(pos,cell)
-        jac,X = self.get_jac(pos,cell,y=y,X=X)
+    # def get_jac(self, pos=None, cell=None, y=None, X=None) -> Tuple[torch.tensor, torch.tensor]:    
+    #     if y is None or X is None:
+    #         y, X = self.get(pos=pos, cell=cell)
+        
+    #     num_batches = y.shape[0]
+    #     N = len(X.pos.flatten())/num_batches
+    #     jac = torch.full((num_batches,N,3), torch.nan)  # Initialize the Jacobian matrix
+        
+    #     # Loop over each batch dimension
+    #     for i in range(num_batches):
+    #         y_batch = y[i]  # Get the i-th batch of y
+    #         # y_batch.backward(retain_graph=True)  # Backpropagate
+    #         # jac[:, i] = X.pos.grad.flatten().detach()  # Store the gradients in the Jacobian matrix
+    #         # X.pos.grad.data.zero_()  # Zero out the gradients for the next iteration
+    #         for n in range(3):
+    #             y_batch[n].backward(retain_graph=True)
+    #             jac[:,n] = X[i,n].pos.grad.flatten().detach()
+    #             X.pos.grad.data.zero_()
+        
+    #     return jac, X
+
+    def get_value_and_jac(self,pos=None,cell=None,X=None)->Tuple[torch.tensor,torch.tensor,Data]:
+        y,X = self.get(pos=pos,cell=cell,X=X)
+        jac,X = self.get_jac(pos=pos,cell=cell,y=y,X=X)
         return y.detach(),jac.detach(),X
 
     def n_parameters(self:T):
