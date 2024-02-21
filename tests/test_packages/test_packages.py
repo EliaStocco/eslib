6ee278a4dbab4abae726dd93424e52a3266366da
EliaStocco <eliastocco@gmail.com>
2024-02-21 16:20:17 +0100

    created test for packages import

--- tests/test_packages/test_packages.py
+++ tests/test_packages/test_packages.py
@@ -6,7 +6,7 @@ import pytest
 def check_package(package_name):
     try:
         importlib.import_module(package_name)
-        print(f"{package_name} is correctly installed.")
+        # print(f"{package_name} is correctly installed.")
         return True
     except ImportError:
         print(f"{package_name} is not installed.")
@@ -26,19 +26,24 @@ def check_imports_in_file(file_path):
 
     return imported_packages
 
-@pytest.mark.my_test
-def main():
+folders = ["."]
+exclude = ["./eslib/functional.py",\
+           "./eslib/old/classes.py"]
+
+@pytest.mark.parametrize("folder", folders)
+def test_packages(folder):
     # Directory containing your project files
-    project_dir = "."
 
     # List to store all imported packages
     all_imported_packages = set()
 
     # Traverse through all Python files in the project directory
-    for root, _, files in os.walk(project_dir):
+    for root, _, files in os.walk(folder):
         for file in files:
             if file.endswith(".py"):
                 file_path = os.path.join(root, file)
+                if file_path in exclude: 
+                    continue
                 imported_packages = check_imports_in_file(file_path)
                 all_imported_packages.update(imported_packages)
 
@@ -51,4 +56,5 @@ def main():
         print("Some required packages are missing.")
 
 if __name__ == "__main__":
-    main()
+    for folder in folders:
+        test_packages(folder)
