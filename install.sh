99c55eb06bb8f8a4fb9853c682eb37ed1a33786b
EliaStocco <eliastocco@gmail.com>
2024-03-13 18:03:21 +0100

    modify PYTHONPATH and PATH

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 pip install -e .
+
+ESLIB="/home/stoccoel/google-personal/codes/eslib"
+export PATH="$PATH:${ESLIB}/eslib"
+export PYTHONPATH="$PYTHONPATH:${ESLIB}/eslib"
+export PATH="$PATH:${ESLIB}"
+export PYTHONPATH="$PYTHONPATH:${ESLIB}"
+for dir in "${ESLIB}"/eslib/scripts/*; do
+    if [ -d "$dir" ]; then
+        export PATH="$PATH:$dir"
+        # echo ${dir}
+        # Make Python files executable in each subdirectory
+        find "$dir" -name "*.py" -type f -exec chmod +x {} \;
+    fi
+done
