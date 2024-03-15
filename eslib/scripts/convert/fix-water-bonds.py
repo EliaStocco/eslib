e64f525ad3d4983402ed94255c27db4e01e9f7e8
EliaStocco <eliastocco@gmail.com>
2024-03-15 16:18:53 +0100

    finished to code 'fix-water-bonds.py'

--- eslib/scripts/convert/fix-water-bonds.py
+++ eslib/scripts/convert/fix-water-bonds.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 from ase.io import read
-from eslib.formatting import esfmt
-from ase import Atoms, Atom
+from eslib.formatting import esfmt, warning
+from ase import Atoms
 import numpy as np
-from ase.io import read
-from ase.neighborlist import NeighborList, neighbor_list
+from ase.io import read, write
+from eslib.tools import cart2frac, frac2cart
 
 #---------------------------------------#
 # Description of the script's purpose
-description = "MACE socket driver."
+description = "Wrap hydrogen atoms such that they will be close to an oxygen atoms."
 
 #---------------------------------------#
 def prepare_args(description):
     import argparse
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
-    parser.add_argument("-i" , "--input"       , **argv, required=True , type=str  , help="file with the atomic structures")
-    parser.add_argument("-if", "--input_format", **argv, required=False, type=str  , help="input file format (default: 'None')" , default=None)
-    parser.add_argument("-rc", "--cutoff"      , **argv, required=False, type=float, help="cutoff/bond lenght [bohr] (default: 3)" , default=3)
-    # parser.add_argument("-m", "--model"     , **argv, required=True , type=str, help="file with the MACE model")
-    # parser.add_argument("-t", "--model_type", **argv, required=False, type=str, help="MACE model data type (default: None)", default=None)
-    # parser.add_argument("-p", "--port"      , **argv, required=True , type=str, help="TCP/IP port number. Ignored when using UNIX domain sockets.")
-    # parser.add_argument("-a", "--address"   , **argv, required=True , type=str, help="Host name (for INET sockets) or name of the UNIX domain socket to connect to.")
-    # parser.add_argument("-u", "--unix"      , **argv, required=True , type=str, help="Use a UNIX domain socket.")
-    # parser.add_argument("-d", "--device"    , **argv, required=True , type=str, help="device (default: 'cpu')", choices=['cpu','gpu','cuda'], default='cpu')
+    parser.add_argument("-i" , "--input"        , **argv, required=True , type=str  , help="file with the atomic structures")
+    parser.add_argument("-if", "--input_format" , **argv, required=False, type=str  , help="input file format (default: 'None')" , default=None)
+    parser.add_argument("-rc", "--cutoff"       , **argv, required=False, type=float, help="cutoff/bond lenght(default: 3)" , default=3)
+    parser.add_argument("-o" , "--output"       , **argv, required=True , type=str  , help="output file with the oxidation numbers (default: 'wrapped.extxyz')", default="wrapped.extxyz")
+    parser.add_argument("-of", "--output_format", **argv, required=False, type=str  , help="output file format (default: None)", default=None)
     return parser.parse_args()
 
 #---------------------------------------#
@@ -35,41 +31,86 @@ def main(args):
     atoms:Atoms = read(args.input,format=args.input_format,index=0)
     print("done")
 
-    # Define a cutoff distance
-    cutoff = args.cutoff  # Angstrom
-
-    # Create a neighbor list
-    nl = NeighborList([cutoff / 2.0] * len(atoms), self_interaction=False, bothways=True)
-
-    # Build the neighbor list
-    nl.update(atoms)
-
+    #------------------#
     # Select oxygen and hydrogen atoms
-    oxygens = [ a for a in atoms if a.symbol == 'O']
+    oxygens   = [ n for n,a in enumerate(atoms) if a.symbol == 'O']
+    hydrogens = [ n for n,a in enumerate(atoms) if a.symbol == 'H']
+    print()
+    print("\t  Number of oxygen atoms: ",len(oxygens))
+    print("\tNumber of hydrigen atoms: ",len(hydrogens))
 
-    # Find hydrogen atoms near oxygen atoms
-    oxygen_hydrogen_pairs = []
 
-    for o_atom in oxygens:
+    #------------------#
+    print("\tWrapping hydrogen atoms:")
+    zeros = np.zeros(3)
+    wrapped = []
+    for o_index in oxygens:
         # Find neighbors of the current oxygen atom within the cutoff distance
-        neighbor_list
-        indices, offsets = nl.get_neighbors(o_atom.index)
         
-        for idx, offset in zip(indices, offsets):
-            if atoms[idx].symbol == 'H':
-                distance = np.linalg.norm(np.dot(offset, atoms.get_cell()))  # Calculate distance with PBC
-                if distance < cutoff:
-                    oxygen_hydrogen_pairs.append((o_atom.index, idx,distance))
+        distances = atoms.get_distances(o_index,hydrogens,mic=True,vector=False)
 
-    # Print the pairs of oxygen and nearby hydrogen atoms
-    for pair in oxygen_hydrogen_pairs:
-        print("Oxygen atom:", pair[0], "Hydrogen atom:", pair[1], "Distance:",pair[2])
+        for n,d in zip(hydrogens,distances):
+            if d > args.cutoff:
+                continue
+            delta = atoms.positions[n] - atoms.positions[o_index] 
+            delta:np.ndarray = cart2frac(atoms.get_cell(),delta)[0]
+            # delta = (2*delta).round(0)/2.
+            delta = delta.round(0).astype(int)
+            if not np.allclose(delta,zeros):
+                print("\t - wrapping hydrogen {:3d} by [{:>2d},{:>2d},{:>2d}]".format(n,*delta.tolist()),\
+                      "(frac. coor.) so that it will be closer to oxygen {:d}".format(o_index))
+                delta = frac2cart(atoms.get_cell(),delta)[0]
+                atoms.positions[n,:] -= delta
+                wrapped.append(n)
+                # print(n," ",delta)
+        
 
-    pass
+    Nwrapping = len(wrapped)
+    Nwrapped  = len(np.unique(wrapped))
+    print("\n\tNumber of wrapping: ",Nwrapping)
+    print("\tNumber of wrapped hydrogens: ",Nwrapped)
+
+    if Nwrapping != Nwrapped:
+        print("\t{:s}: the previous two numbers are expected to be the same. Carefully check your input and output files.".format(warning))
+
+
+    print("\n\tWriting (un)wrapped atomic structure to file '{:s}' ... ".format(args.output), end="")
+    try:
+        write(images=atoms,filename=args.output,format=args.output_format) # fmt)
+        print("done")
+    except Exception as e:
+        print("\n\tError: {:s}".format(e))
 
 #---------------------------------------#
 if __name__ == "__main__":
     main()
 
+# {
+#     // Use IntelliSense to learn about possible attributes.
+#     // Hover to view descriptions of existing attributes.
+#     // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+#     "version": "0.2.0",
+#     "configurations": [
+#         {
+#             "name": "Python: Current File",
+#             "type": "debugpy",
+#             "request": "launch",
+#             "program": "/home/stoccoel/google-personal/codes/eslib/eslib/scripts/convert/fix-water-bonds.py",
+#             "cwd" : "/home/stoccoel/google-personal/works/water/MACE/",
+#             "console": "integratedTerminal",
+#             "justMyCode": false,
+#             "args": [
+#                 "-i", "MACE_test.extxyz", 
+#                 // "-m", "model/dipole_32.model", 
+#                 // "-p", "your_port_number", 
+#                 // "-a", "your_address", 
+#                 // "-d", "cpu"
+#             ],
+#             "env": {
+#                 "PYTHONPATH": "/home/stoccoel/google-personal/codes/mace/mace/"
+#             }
+#         }
+#     ]
+# }
 
 
