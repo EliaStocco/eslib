d09281f1552126f71a5b727b82736f2118e156f4
EliaStocco <eliastocco@gmail.com>
2024-03-19 11:47:16 +0100

    added 'remove_replicas' option

--- eslib/scripts/text/aims2dipole.py
+++ eslib/scripts/text/aims2dipole.py
@@ -2,6 +2,7 @@
 import re
 import numpy as np
 from eslib.tools import convert
+from eslib.input import str2bool
 
 #---------------------------------------#
 # Description of the script's purpose
@@ -33,6 +34,7 @@ def prepare_args():
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
     parser.add_argument("-i", "--input"         , **argv,type=str, help="input txt file")
+    parser.add_argument("-rr" , "--remove_replicas", **argv,required=False, type=str2bool, help='whether to remove replicas (default: false)', default=False)
     parser.add_argument("-o", "--output"        , **argv,type=str, help="output file with the dipole values (default: 'dipole.aims.txt')", default="dipole.aims.txt")
     parser.add_argument("-of", "--output_format", **argv,type=str, help="output format for np.savetxt (default: '%%24.18f')", default='%24.18f')
     return parser.parse_args()
@@ -56,10 +58,17 @@ def main():
     # Open the input file for reading and the output file for writing
     factor = convert(1,"length","angstrom","atomic_unit")
     n = 0 
+    dipoles = []
+    steps = []
+    step = re.compile(r"Step:\s+(\d+)")
     pattern = re.compile(r'Total dipole moment.*?([-+]?\d*\.\d+(?:[eE][-+]?\d+)?).*?([-+]?\d*\.\d+(?:[eE][-+]?\d+)?).*?([-+]?\d*\.\d+(?:[eE][-+]?\d+)?)')
-    with open(args.input, 'r') as input_file, open(args.output, 'w') as output_file:
+    with open(args.input, 'r') as input_file:
         # Iterate through each line in the input file
         for line in input_file:
+            test = step.search(line)
+            if test is not None:
+                steps.append(int(test.group(1)))
+
             if "Total dipole moment" in line and "[eAng]" in line:
                 # Search for the pattern and extract the first three float values
                 # matches = re.search(r"Total dipole moment \[eAng\]\s*:\s*([-+]?\d*\.\d+|\d+\.\d*|\d+)", line)
@@ -72,10 +81,23 @@ def main():
                 if matches:
                     float_values = [float(match) for match in matches.groups()[:3]]
                     float_values = np.asarray(float_values).reshape((1,3))
-                    np.savetxt(output_file,factor*float_values,fmt=args.output_format)
-                    n += 1 
+                    dipoles.append(float_values)
+                    # np.savetxt(output_file,factor*float_values,fmt=args.output_format)
+                    # n += 1 
                     # output_file.write(','.join(map(str, float_values)) + '\n')  # Save the values as a comma-separated line
-    print("\tN. of dipole values found: ",n)
+    print("\tN. of dipole values found: ",len(dipoles))
+
+    #------------------#
+    steps= np.asarray(steps)
+    test, indices = np.unique(steps, return_index=True)
+    if steps.shape != test.shape and not args.remove_replicas:
+        print("\t{:s}: there could be replicas. Specify '-rr/--remove_replicas true' to remove them.".format(warning))
+    if args.remove_replicas:
+        dipoles = [dipoles[index] for index in indices]
+
+    with open(args.output, 'w') as output_file:
+        dipoles = np.asarray(dipoles).reshape(-1,3)
+        np.savetxt(output_file,factor*dipoles,fmt=args.output_format)
 
     #------------------#
     # Script completion message
--- eslib/scripts/text/extras2dipole.py
+++ eslib/scripts/text/extras2dipole.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import json
-from eslib.formatting import esfmt, float_format
+from eslib.formatting import esfmt, float_format, warning
+from eslib.input import str2bool
 import numpy as np
+import re
 
 #---------------------------------------#
 # Description of the script's purpose
@@ -13,6 +15,7 @@ def prepare_args(description):
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
     parser.add_argument("-i" , "--input" , **argv, required=True , type=str, help="input file with the JSOn formatted dipoles [au]")
+    parser.add_argument("-rr" , "--remove_replicas", **argv,required=False, type=str2bool, help='whether to remove replicas (default: false)', default=False)
     parser.add_argument("-o" , "--output", **argv, required=False, type=str, help="txt output file with dipoles (default: 'dipoles.txt')", default='dipoles.txt')
     return parser.parse_args()
 
@@ -28,9 +31,16 @@ def main(args):
 
     # Initialize an empty list to store dipole values
     dipoles = []
+    steps = []
+    step = re.compile(r"Step:\s+(\d+)")
+    # strings = [ step.search(comment).group(1) for comment in comments ]
+    #         steps = np.asarray([int(i) for i in strings],dtype=int)
 
     # Iterate through each line in the file
     for line in lines:
+        test = step.search(line)
+        if test is not None:
+            steps.append(int(test.group(1)))
         # Check if the line contains dipole data
         if '"dipole"' in line:
             # Extract the dipole values from the line
@@ -38,6 +48,14 @@ def main(args):
             # Append the dipole values to the list
             dipoles.append(dipole_data)
     print("done")
+
+    #------------------#
+    steps= np.asarray(steps)
+    test, indices = np.unique(steps, return_index=True)
+    if steps.shape != test.shape and not args.remove_replicas:
+        print("\t{:s}: there could be replicas. Specify '-rr/--remove_replicas true' to remove them.".format(warning))
+    if args.remove_replicas:
+        dipoles = [dipoles[index] for index in indices]
 
     #------------------#
     print("\tConverting dipoles to np.array: ... ", end="")
