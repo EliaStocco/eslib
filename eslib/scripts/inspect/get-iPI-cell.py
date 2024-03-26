673e521743bfa12947bd2057200e8e8989358a45
EliaStocco <eliastocco@gmail.com>
2024-03-26 15:05:27 +0100

    formatting script

--- eslib/scripts/inspect/get-iPI-cell.py
+++ eslib/scripts/inspect/get-iPI-cell.py
@@ -2,66 +2,30 @@
 # author: Elia Stocco
 # email : elia.stocco@mpsd.mpg.de
 from ase.io import read
-import argparse
 import numpy as np
 from eslib.formatting import matrix2str
+from eslib.formatting import esfmt
 
 #---------------------------------------#
-description     = "Save to file and/or print to screen the cell an atomic configuration in a i-PI compatible format."
-warning         = "***Warning***"
-closure         = "Job done :)"
-keywords        = "It's up to you to modify the required keywords."
-input_arguments = "Input arguments"
+description = "Save to file and/or print to screen the cell an atomic configuration in a i-PI compatible format."
 
 #---------------------------------------#
-# colors
-try :
-    import colorama
-    from colorama import Fore, Style
-    colorama.init(autoreset=True)
-    description     = Fore.GREEN  + Style.BRIGHT + description             + Style.RESET_ALL
-    warning         = Fore.MAGENTA    + Style.BRIGHT + warning.replace("*","") + Style.RESET_ALL
-    closure         = Fore.BLUE   + Style.BRIGHT + closure                 + Style.RESET_ALL
-    keywords        = Fore.YELLOW + Style.NORMAL + keywords                + Style.RESET_ALL
-    input_arguments = Fore.GREEN  + Style.NORMAL + input_arguments         + Style.RESET_ALL
-except:
-    pass
-
-#---------------------------------------#
-def prepare_parser():
+def prepare_parser(description):
+    import argparse
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar":"\b"}
-    parser.add_argument(
-        "-i", "--input",  type=str,**argv,
-        help="atomic structure input file"
-    )
-    parser.add_argument(
-        "-d", "--digits",  type=int,**argv,
-        help="number of digits (default: 6)", default=6
-    )
-    parser.add_argument(
-        "-e", "--exponential",  type=bool,**argv,
-        help="exponential notation (default: False)", default=False
-    )
-    parser.add_argument(
-        "-o", "--output",  type=str,**argv,
-        help="output file (default: None)", default=None
-    )
-    options = parser.parse_args()
-    return options
+    parser.add_argument("-i", "--input",  type=str,**argv,help="atomic structure input file")
+    parser.add_argument("-d", "--digits",  type=int,**argv,help="number of digits (default: 6)", default=6)
+    parser.add_argument("-m", "--mode"  ,  type=int,**argv,help="mode (default: None)", default=None)
+    parser.add_argument("-e", "--exponential",  type=bool,**argv,help="exponential notation (default: False)", default=False)
+    parser.add_argument("-o", "--output",  type=str,**argv,help="output file (default: None)", default=None)
+    return parser.parse_args()
 
 #---------------------------------------#
-def main():
+@esfmt(prepare_parser,description)
+def main(args):
 
-    args = prepare_parser()
-
-    # Print the script's description
-    print("\n\t{:s}".format(description))
-    # print("done")
-    print("\n\t{:s}:".format(input_arguments))
-    for k in args.__dict__.keys():
-        print("\t{:>20s}:".format(k),getattr(args,k))
-    print()
+    otext = ""
 
     # structure A
     print("\tReading atomic structure from input file '{:s}' ... ".format(args.input), end="")
@@ -77,25 +41,39 @@ def main():
         cell = np.asarray(atoms.cell).T
 
     print("\tCell:")
-    line = matrix2str(cell,col_names=["1","2","3"],cols_align="^",width=6)
+    line = matrix2str(cell,col_names=["1","2","3"],cols_align="^",width=args.digits+2)
     print(line)
 
-    print("\tCell in the i-PI compatible format:")
+    print("\tCell in i-PI compatible formats:")
+    print("\tmanual:")
     exp = "e" if args.exponential else "f" 
-    num_format = f'{{:.{args.digits}{exp}}}'
-    text_format = "[ " + (num_format+", ")*8 + num_format + "]"
-    text = text_format.format(*cell.flatten().tolist())
-    print("\t"+text)
+    num_format = f'{{:>{args.digits+6}.{args.digits}{exp}}}'
+    text_format =   "\t<cell mode='manual' units=' ... ' > \n" + \
+                    ( "\t\t" + (num_format+", ")*3 + "\n")*2  + "\t\t" + (num_format+", ")*2  + num_format + \
+                    "\n\t</cell>"
+    text =  text_format.format(*cell.flatten().tolist())
+    print(text)
+    if args.mode is None or args.mode == "manual":
+        otext += text
+
+    print("\tabcABC:")
+    exp = "e" if args.exponential else "f" 
+    num_format = f'{{:>{args.digits+6}.{args.digits}{exp}}}'
+    text_format =   "\t<cell mode='abcABC' units=' ... ' > \n" + \
+                    "\t\t" + (num_format+", ")*3 + "\n"  + "\t\t" + (num_format+", ")*2  + num_format + \
+                    "\n\t</cell>"
+    text =  text_format.format(*atoms.get_cell().cellpar().tolist())
+    print(text)
+    if args.mode is None or args.mode == "abcABC":
+        otext += text
     
     if args.output is not None:
         print("\n\tWriting cell in the i-PI compatible format to file '{:s}' ... ".format(args.output), end="")
         # Open a file in write mode ('w')
         with open(args.output, 'w') as file:
             # Write a string to the file
-            file.write(text)
+            file.write(otext)
         print("done")
-    
-    print("\n\t{:s}\n".format(closure))
 
 if __name__ == "__main__":
     main()
