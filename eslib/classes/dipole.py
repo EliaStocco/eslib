2914c9b279048fe300ab7a2d8cd8bc2b5c058609
EliaStocco <eliastocco@gmail.com>
2024-04-10 17:02:24 +0200

    NN as dipole model

--- eslib/classes/dipole.py
+++ eslib/classes/dipole.py
@@ -8,6 +8,7 @@ from eslib.tools import cart2frac
 from eslib.physics import compute_dipole_quanta
 from copy import copy
 from collections import Counter
+from mace.calculators import MACEliaCalculator
 
 class DipoleModel(pickleIO):
     def get(self,traj:List[Atoms],**argv):
@@ -207,3 +208,47 @@ class DipoleLinearModel(DipoleModel):
 
     def get_reference(self):
         return self.ref.copy()
+    
+from mace.tools import torch_geometric, torch_tools
+from mace.cli.elia_configs import make_dataloader
+from mace.modules.models import get_model
+@dataclass
+class DipoleMACECalculator(DipoleModel):
+    default_dtype: str
+    device: str
+    model: str
+    model_type: str
+    batch_size: int
+    charges_key: str
+
+    def __post_init__(self):
+        self.initialize()
+
+    def initialize(self):        
+        torch_tools.set_default_dtype(self.default_dtype)
+        self.device = torch_tools.init_device([self.device])
+        self.network = get_model(model_path=self.model,
+                               model_type=self.model_type,
+                               device=self.device)
+        self.network = self.network.to(self.device)  # shouldn't be necessary but seems to help with CUDA problems
+        for param in self.network.parameters():
+            param.requires_grad = False
+        
+    def get(self,traj:List[Atoms],**argv):
+        self.initialize()
+        data_loader:torch_geometric.dataloader.DataLoader = \
+            make_dataloader(atoms_list=traj,
+                            model=self.network,
+                            batch_size=self.batch_size,
+                            charges_key=self.charges_key)
+        k = 0
+        dipoles = np.zeros((len(traj),3))
+        for batch in data_loader:
+            batch = batch.to(self.device)
+            output:dict = self.network(batch.to_dict(), compute_stress=False)
+            output = output["dipole"]
+            N = len(output)
+            dipoles[k:k+N] = torch_tools.to_numpy(output)
+            k += N 
+
+        return dipoles
