181ec2dec033204bfbfe80516733858697f56e20
EliaStocco <eliastocco@gmail.com>
2024-04-17 11:38:24 +0200

    added new methods

--- eslib/classes/dipole/DipoleModel.py
+++ eslib/classes/dipole/DipoleModel.py
@@ -1,10 +1,18 @@
 from ase import Atoms
-from typing import List, Dict
+from typing import List
 from eslib.classes.io import pickleIO
-# from abc import ABC, abstractmethod
+from typing import Type, TypeVar
+T = TypeVar('T', bound='DipoleModel')
 
 class DipoleModel(pickleIO):
     # @abstractmethod
     def get(self,traj:List[Atoms],**argv):
         raise NotImplementedError("this method should be overwritten.")
         pass
+
+    @classmethod
+    def from_file(cls: Type[T], file_path: str) -> T:
+        return super().from_pickle(file_path)
+    
+    def summary(self, string="\t"):
+        return
