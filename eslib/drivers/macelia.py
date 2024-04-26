fc3d4eebe12af961803cf2ec4b212269fd69cffe
EliaStocco <eliastocco@gmail.com>
2024-04-26 14:24:08 +0200

    user can choose the socket client

--- eslib/drivers/macelia.py
+++ eslib/drivers/macelia.py
@@ -6,7 +6,7 @@ from eslib.formatting import esfmt
 from eslib.functions import suppress_output
 from eslib.input import str2bool
 from eslib.drivers.socketextras import SocketClientExtras
-# from ase.calculators.socketio import SocketClient
+from ase.calculators.socketio import SocketClient
 
 #---------------------------------------#
 # Description of the script's purpose
@@ -17,16 +17,17 @@ def prepare_args(description):
     import argparse
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
-    parser.add_argument("-s" , "--structure" , **argv, required=True , type=str     , help="file with the atomic structure")
-    parser.add_argument("-f" , "--format"    , **argv, required=False, type=str     , help="file format of the atomic structure (default: 'None')" , default=None)
-    parser.add_argument("-m" , "--model"     , **argv, required=False, type=str     , help="file with the MACE model")
-    parser.add_argument("-mt", "--model_type", **argv, required=True , type=str     , help="MACE model data type (default: None)", default=None)
-    parser.add_argument("-p" , "--port"      , **argv, required=False, type=int     , help="TCP/IP port number. Ignored when using UNIX domain sockets.")
-    parser.add_argument("-a" , "--address"   , **argv, required=True , type=str     , help="Host name (for INET sockets) or name of the UNIX domain socket to connect to.")
-    parser.add_argument("-u" , "--unix"      , **argv, required=False, type=str2bool, help="Use a UNIX domain socket (default: false)", default=False)
-    parser.add_argument("-d" , "--device"    , **argv, required=False, type=str     , help="device (default: 'cpu')", choices=['cpu','gpu','cuda'], default='cpu')
-    parser.add_argument("-dt", "--dtype"     , **argv, required=False, type=str     , help="dtype (default: 'float64')", choices=['float64','float32'], default='float64')
-    return parser# .parse_args()
+    parser.add_argument("-s" , "--structure"    , **argv, required=True , type=str     , help="file with the atomic structure")
+    parser.add_argument("-f" , "--format"       , **argv, required=False, type=str     , help="file format of the atomic structure (default: 'None')" , default=None)
+    parser.add_argument("-m" , "--model"        , **argv, required=False, type=str     , help="file with the MACE model")
+    parser.add_argument("-mt", "--model_type"   , **argv, required=True , type=str     , help="MACE model data type (default: None)", default=None)
+    parser.add_argument("-p" , "--port"         , **argv, required=False, type=int     , help="TCP/IP port number. Ignored when using UNIX domain sockets.")
+    parser.add_argument("-a" , "--address"      , **argv, required=True , type=str     , help="Host name (for INET sockets) or name of the UNIX domain socket to connect to.")
+    parser.add_argument("-u" , "--unix"         , **argv, required=False, type=str2bool, help="Use a UNIX domain socket (default: false)", default=False)
+    parser.add_argument("-d" , "--device"       , **argv, required=False, type=str     , help="device (default: 'cpu')", choices=['cpu','gpu','cuda'], default='cpu')
+    parser.add_argument("-dt", "--dtype"        , **argv, required=False, type=str     , help="dtype (default: 'float64')", choices=['float64','float32'], default='float64')
+    parser.add_argument("-sc", "--socket_client", **argv, required=False, type=str     , help="socket client (default: 'eslib')", choices=['eslib','ase'], default='eslib')
+    return parser
 
 #---------------------------------------#
 @esfmt(prepare_args,description)
@@ -79,8 +80,9 @@ def main(args):
     atoms.calc = calculator # atoms.set_calculator(calculator)
 
     #------------------#
+    socket_client = SocketClientExtras if args.socket_client == 'eslib' else SocketClient
     print("\tPreparing the socket communication ... ", end="")
-    client = SocketClientExtras(host=args.address,\
+    client = socket_client(host=args.address,\
                           port=args.port,\
                           unixsocket=args.address if args.unix else None)
     print("done")
