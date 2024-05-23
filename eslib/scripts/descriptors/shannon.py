aeb39769cdcd077324508c5ce79f098600dabdff
EliaStocco <eliastocco@gmail.com>
2024-05-23 15:04:29 +0200

    commented code

--- eslib/scripts/descriptors/shannon.py
+++ eslib/scripts/descriptors/shannon.py
@@ -25,51 +25,10 @@ def prepare_args(description):
     parser.add_argument("-p"  , "--plot"            , type=str     , required=False, **argv, help="plot (default: 'entropy.pdf')", default='entropy.pdf')
     return parser
 
-
-
-# @njit(parallel=True)
-# def histogram_along_axis(data: np.ndarray, bins: int, axis: int) -> np.ndarray:
-#     """
-#     Compute the histogram of a numpy array along a specific axis.
-
-#     Parameters:
-#         data (numpy.ndarray): The input array.
-#         bins (int): The number of bins for the histogram.
-#         axis (int): The axis along which to compute the histogram.
-
-#     Returns:
-#         numpy.ndarray: The histogram values along the specified axis.
-
-#     Example:
-#         >>> data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-#         >>> bins = 5
-#         >>> axis = 1
-#         >>> hist = histogram_along_axis(data, bins, axis)
-#         >>> print(hist)
-#         array([[1, 1, 1, 0, 0],
-#                [1, 1, 1, 0, 0],
-#                [1, 1, 1, 0, 0]])
-#     """
-#     # Transpose the array to move the specified axis to the first position
-#     data_transposed = np.transpose(data, np.roll(np.arange(data.ndim), -axis))
-
-#     # Compute the histogram along the first axis
-#     range = (np.min(data), np.max(data))
-#     hist = np.zeros((bins,) + data_transposed.shape[1:], dtype=np.int64)
-#     for i in prange(data_transposed.shape[1]):
-#         hist[:, i] = np.histogram(data_transposed[:, i], bins=bins, range=range)[0]
-
-#     # Transpose the result back to the original shape
-#     hist_transposed = np.transpose(hist, np.roll(np.arange(hist.ndim), axis))
-
-#     return hist_transposed
-
 #---------------------------------------#
 @esfmt(prepare_args, description)
 def main(args):
 
-    #------------------#
-    # assert args.n_bins < args.block_length
 
     #------------------#
     print("\n\tReading SOAP descriptors from file '{:s}' ... ".format(args.soap_descriptors),end="")
@@ -94,20 +53,20 @@ def main(args):
     print("\n\tComputing the histogram of the SOAP descriptors ... ",end="")
     hist = [None]*len(args.n_bins)
     for n,nbins in enumerate(args.n_bins):
-        # nbins = args.n_bins[n]
         hist[n] = histogram_along_axis(X.data,nbins,X.dims.index("structures"))
-    # hist = np.asarray(hist)
-    # hist = np.moveaxis(hist, 0, -1)
+    # every histogram has a different shape which depends on nbins
     hist = [ xr.DataArray(h,dims=("dataset","x","descriptors")) for h in hist]
     print("done")
     print("\thist.shape: ",hist[0].shape)
 
     #------------------#
+    # accumulate the counts such that the last dataset will have information about all the previous datasets
     print("\n\tAccumulatig histograms ... ",end="")
     hist = [ np.cumsum(h, axis=h.dims.index("dataset")) for h in hist ] 
     print("done")
     print("\thist.shape: ",hist[0].shape)
 
+    # transfrom the distribution into a binary array
     for h in hist:
         h.data[h.data > 0] = 1
     hist = [ xr.DataArray(h) for h in hist] 
@@ -119,17 +78,17 @@ def main(args):
     SE = np.full(shape,np.nan)
 
     for b,h in enumerate(hist): # cycle over nbins
-        # for n,p in h.coords["dataset"].items(): # cycle over dataset
         for n,p in enumerate(h): # cycle over dataset
-            # p:xr.DataArray = hist.isel(dataset=n)
             SE[n,:,b] = entropy(p.data,axis=p.dims.index("x"))
 
     SE = xr.DataArray(SE,dims=("dataset","descriptors","bins"))
+    # simple mean over the descriptors, it could be modified/improved
     SE = SE.mean('descriptors')
     print("done")
     print("\tShannon entropy shape: ",SE.shape)
 
     #------------------#
+    # convert into a pandas dataframe
     df = SE.to_dataframe(name="entropy").reset_index()
     df['size'] = (df['dataset']+1)*args.block_length
     df['bins'] = args.n_bins[df['bins']]
@@ -138,7 +97,7 @@ def main(args):
     print("done")
 
     #------------------#
-    # Group DataFrame by 'nbins' column
+    # plot
     groups = df.groupby('bins')
 
     fig,axes = plt.subplots(ncols=1,nrows=2,figsize=(10,6),sharex=True)
@@ -153,8 +112,6 @@ def main(args):
         ax.grid(True)
 
     axes[1].set_xlabel('Dataset size')
-    # axes[1].set_xscale('log')
-    # axes[1].set_yscale('log')
     axes[0].set_ylabel('H')
     axes[1].set_ylabel('-$\\Delta$H')
     axes[0].set_title('Shannon entropy H')
