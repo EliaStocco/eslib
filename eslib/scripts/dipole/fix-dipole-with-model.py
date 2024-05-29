e2292f5f57afd9f00cc27474ab81f19ce5161f80
EliaStocco <eliastocco@gmail.com>
2024-05-29 17:34:21 +0200

    tidied up

--- eslib/scripts/dipole/fix-dipole-with-model.py
+++ eslib/scripts/dipole/fix-dipole-with-model.py
@@ -4,29 +4,30 @@ import matplotlib.pyplot as plt
 from eslib.plot import plot_bisector
 from eslib.classes.dipole import DipoleModel, DipoleLinearModel
 from eslib.classes.trajectory import AtomicStructures
-from eslib.tools import cart2lattice, cart2frac, frac2cart
+from eslib.tools import frac2cart
 from eslib.output import output_folder
-from ase.io import write
-from eslib.formatting import esfmt, everythingok, warning, error
+from eslib.formatting import esfmt, everythingok, warning, float_format
+from eslib.physics import compute_dipole_quanta
 
 #---------------------------------------#
 # Description of the script's purpose
 description = "Fix the dipole jumps using a (previously created) linear model."
 
 #---------------------------------------#
-def correlation_plot(A,B,nameA,nameB,file):
+def correlation_plot(A,B,nameA,nameB,file,bisectors:bool=True):
     fig,axes = plt.subplots(ncols=3,figsize=(15,5))
 
     labels = ["x","y","z"]
     for n,ax in enumerate(axes):
         ax.scatter(A[:,n],B[:,n],label=labels[n])
         ax.grid()
-        xlims = ax.get_xlim()
-        ylims = ax.get_ylim()
-        for n in range(-20,20):
-            plot_bisector(ax,n)
-        ax.set_xlim(*xlims)
-        ax.set_ylim(*ylims)
+        if bisectors:
+            xlims = ax.get_xlim()
+            ylims = ax.get_ylim()
+            for n in range(-20,20):
+                plot_bisector(ax,n)
+            ax.set_xlim(*xlims)
+            ax.set_ylim(*ylims)
         
     axes[1].set_xlabel(nameA)
     axes[0].set_ylabel(nameB)
@@ -64,14 +65,6 @@ def main(args):
     dft = trajectory.get(args.in_dipole)
     print("done")
 
-    # #---------------------------------------#
-    # # lattice vectors
-    # print("\tExtracting the lattice vectors from the trajectory ... ", end="")
-    # def get_cell(e:Atoms):
-    #     return e.get_cell()
-    # lattices = trajectory.call(get_cell)
-    # print("done")
-
     #------------------#
     # linear model
     print("\tLoading the dipole linear model from file '{:s}' ... ".format(args.model), end="")
@@ -92,7 +85,7 @@ def main(args):
     #------------------#
     # dipole
     print("\n\tComputing the dipoles using the linear model ... ", end="")
-    linear = model.get(list(trajectory))
+    linear = model.get(trajectory)
     print("done")
 
     #------------------#
@@ -102,34 +95,33 @@ def main(args):
     if args.folder is not None:
         print()
         file = "{:s}/dipole.linear-model.txt".format(args.folder)
-        print("\tSaving dipoles computed from the linear model in file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,linear)
+        print("\tSaving dipoles computed from the linear model to file '{:s}' ... ".format(file), end="")
+        np.savetxt(file,linear,fmt=float_format)
         print("done")
 
         file = "{:s}/dipole.dft.txt".format(args.folder)
-        print("\tSaving dipoles computed from DFT in file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,dft)
+        print("\tSaving dipoles computed from DFT to file '{:s}' ... ".format(file), end="")
+        np.savetxt(file,dft,fmt=float_format)
         print("done")
 
-        # file = "{:s}/dipole.correlation.pdf".format(args.folder)
-        # print("\tSaving correlation plot between DFT and LM dipole to file '{:s}' ... ".format(file), end="")        
-        # correlation_plot(dft,linear,"DFT","LM",file)
-        # print("done")
+        file = "{:s}/dipole.correlation.pdf".format(args.folder)
+        print("\tSaving correlation plot between DFT and LM dipole to file '{:s}' ... ".format(file), end="")        
+        correlation_plot(dft,linear,"DFT","LM",file,bisectors=False)
+        print("done")
 
     #------------------#
+        
     # quanta
     print("\n\tComputing the dipole quanta ... ", end="")
-    N = len(trajectory)
+    _,quanta_dft = compute_dipole_quanta(trajectory,in_keyword=args.in_dipole)
+    model_traj = trajectory.copy()
+    model_traj.set(args.in_dipole,linear,"info")
+    _,quanta_model = compute_dipole_quanta(model_traj,in_keyword=args.in_dipole)
     quanta = {
-        "DFT" : np.zeros((N,3)),
-        "LM"  : np.zeros((N,3))
+        "DFT" : quanta_dft,
+        "LM"  : quanta_model
     } 
-    cell = trajectory[0].get_cell()#lattices[n]
-    length = cell.cellpar()[0:3]
-    R = cart2lattice(cell)
-    for n in range(N):       
-        quanta["DFT"][n,:] = R @ dft[n]    / length
-        quanta["LM"][n,:]  = R @ linear[n] / length
+    del model_traj
     print("done")
 
     #------------------#
@@ -142,107 +134,96 @@ def main(args):
 
         file = "{:s}/quanta.dft.txt".format(args.folder)
         print("\tSaving quanta computed from DFT to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,quanta["DFT"])
+        np.savetxt(file,quanta["DFT"],fmt=float_format)
         print("done")
 
         file = "{:s}/quanta.lm.txt".format(args.folder)
         print("\tSaving quanta computed from LM to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,quanta["LM"])
+        np.savetxt(file,quanta["LM"],fmt=float_format)
         print("done")
 
         file = "{:s}/factors.float.txt".format(args.folder)
         print("\tSaving differences between DFT and LM dipoles to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,factor)
+        np.savetxt(file,factor,fmt=float_format)
         print("done")
 
         file = "{:s}/factors.int.txt".format(args.folder)
         print("\tSaving (rounded to integers) differences between DFT and LM dipoles to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,intfactor)
+        np.savetxt(file,intfactor,fmt='%d')
         print("done")
 
         file = "{:s}/factors.diff.txt".format(args.folder)
         print("\tSaving differences between teh previous two saved quantities to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,factor-intfactor)
+        np.savetxt(file,factor-intfactor,fmt=float_format)
         print("done")
 
         file = "{:s}/quanta.correlation.pdf".format(args.folder)
         print("\tSaving correlation plot between DFT and LM quanta to file '{:s}' ... ".format(file), end="")        
-        correlation_plot(quanta["DFT"],quanta["LM"],"DFT","LM",file)
+        correlation_plot(quanta["DFT"],quanta["LM"],"DFT","LM",file,bisectors=True)
         print("done")
 
     #------------------#
     # correction
-    
     print("\n\tCorrecting the DFT dipoles ... ", end="")
-    N = len(trajectory)
-    fixed_dipole = np.full((N,3),np.nan)
-    cell = trajectory[0].get_cell()#lattices[n]
-    length = cell.cellpar()[0:3]
-    
-    # R = lattice2cart(cell)
-    # fixed_dipole = ( R @ (quanta["DFT"] -  intfactor).T ).T* length
-    
-    # The previous code lines should be the same as the following:
-    fixed_dipole = frac2cart(cell=trajectory[0].get_cell(),v=(quanta["DFT"] -  intfactor))
+    fixed_dipole = np.full((len(trajectory),3),np.nan)
+    for n,atoms in enumerate(trajectory):
+        v = quanta["DFT"][n,:] -  intfactor[n,:]
+        assert v.shape == (3,)
+        fixed_dipole[n,:] = frac2cart(cell=atoms.get_cell(),v=v)
     print("done")
+
+    print("\n\tn. of corrected values along:")
+    print("\t\t1st lattice vector: ",np.count_nonzero(intfactor[:,0]))
+    print("\t\t2nd lattice vector: ",np.count_nonzero(intfactor[:,1]))
+    print("\t\t3rd lattice vector: ",np.count_nonzero(intfactor[:,2]))
 
     #------------------#
     # set info
-    print("\tAdding fixed dipole as info '{:s}' and quanta as 'quanta' to atomic structures ... ".format(args.out_dipole), end="")
-    for n in range(N):
-        trajectory[n].info[args.out_dipole] = fixed_dipole[n,:]
-        _quanta = cart2frac(cell=trajectory[n].get_cell(),v=fixed_dipole[n,:])
-        trajectory[n].info["quanta"] = _quanta.flatten()
+    print("\n\tAdding fixed dipole as info '{:s}'  to atomic structures ... ".format(args.out_dipole), end="")
+    trajectory.set(args.out_dipole,fixed_dipole,"info")
+    assert np.allclose(trajectory.get(args.out_dipole),fixed_dipole)
     print("done")
 
-    # #------------------#
-    # print("\tLooking for outliers ... ", end="")
-    # for n in range(N):
-    #     fd = trajectory[n].info["dipole"]
-    #     fd_quanta  = cart2frac(cell=trajectory[n].get_cell(),v=fd)
-    #     lm_quanta = cart2frac(cell=trajectory[n].get_cell(),v=linear[n])
-    #     # norm = np.sqrt(np.square(fd_quanta-lm_quanta).sum())
-    #     # norm = np.linalg.norm(fd-linear[n])
-    #     # print(norm)
-    # print("done")
+
+    _,fixed_quanta =  compute_dipole_quanta(trajectory,in_keyword=args.out_dipole)
+    factor = np.asarray(fixed_quanta - quanta["LM"])
+    intfactor = np.round(factor)
+    assert np.sum(np.abs(intfactor)) == 0
+    assert trajectory.is_there(args.out_dipole)
+
 
     #------------------#
     # writing
     print("\n\tWriting output to file '{:s}' ... ".format(args.output), end="")
-    try:
-        write(args.output, list(trajectory), format="extxyz") # fmt)
-        print("done")
-    except Exception as e:
-        print(f"\n\t{error}: {e}")
-
-    # #------------------#
-    # # output
-    # print("\tSaving the fixed dipoles to file '{:s}' ... ".format(args.output), end="")
-    # np.savetxt(args.output,fixed_dipole,fmt='%24.18e')
-    # print("done")
+    trajectory.to_file(file=args.output,format="extxyz")
+    print("done")
 
     if args.folder is not None:
         print()
 
-        print("\tComputing the quanta of the fixed dipoles ... ", end="")
-        R = cart2lattice(cell)
-        fixed_quanta = (R @ fixed_dipole.T).T / length
-        print("done")
-
         file = "{:s}/quanta.fixed.txt".format(args.folder)
         print("\tSaving quanta of the fixed dipoles to file '{:s}' ... ".format(file), end="")
-        np.savetxt(file,fixed_quanta)
+        _,fixed_quanta = compute_dipole_quanta(trajectory,in_keyword=args.out_dipole)
+        np.savetxt(file,fixed_quanta,fmt=float_format)
         print("done")
 
         file = "{:s}/quanta-fixed.correlation.pdf".format(args.folder)
         print("\tSaving correlation plot between fixed DFT and LM quanta to file '{:s}' ... ".format(file), end="")        
-        correlation_plot(fixed_quanta,quanta["LM"],"DFT","LM",file)
+        correlation_plot(fixed_quanta,quanta["LM"],"DFT","LM",file,bisectors=True)
         print("done")
 
-        # file = "{:s}/dipole-fixed.correlation.pdf".format(args.folder)
-        # print("\tSaving correlation plot between fixed DFT and LM dipole to file '{:s}' ... ".format(file), end="")        
-        # correlation_plot(fixed_dipole,linear,"DFT","LM",file)
-        # print("done")
+        file = "{:s}/dipole-fixed.correlation.pdf".format(args.folder)
+        print("\tSaving correlation plot between fixed DFT and LM dipole to file '{:s}' ... ".format(file), end="")        
+        _fixed_dipole = trajectory.get(args.out_dipole)
+        correlation_plot(_fixed_dipole,linear,"DFT","LM",file,bisectors=False)
+        print("done")
+        
+        assert np.allclose(_fixed_dipole,fixed_dipole)
+
+        file = "{:s}/dipole.fixed.txt".format(args.folder)
+        print("\tSaving the fixed dipoles to file '{:s}' ... ".format(args.output), end="")
+        np.savetxt(file,fixed_dipole,fmt=float_format)
+        print("done")
 
 #---------------------------------------#
 if __name__ == "__main__":
