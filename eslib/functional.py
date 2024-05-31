cfb3ecaf47e2a2063d7716d6071942ee9faf7ab8
EliaStocco <eliastocco@gmail.com>
2024-05-31 10:44:28 +0200

    added docstring

--- eslib/functional.py
+++ eslib/functional.py
@@ -2,6 +2,22 @@ import functools
 import warnings
 
 def deprecated(reason):
+    """
+    A decorator to mark functions as deprecated. It will result in a warning being emitted 
+    when the function is used.
+
+    Args:
+        reason (str): A message that describes the reason why the function is deprecated.
+
+    Returns:
+        function: A decorator that wraps the original function and emits a deprecation warning 
+        when it is called.
+
+    Example:
+        @deprecated("use 'new_function' instead")
+        def old_function(x, y):
+            return x + y
+    """
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
