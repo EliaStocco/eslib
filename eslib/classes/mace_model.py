fa70fcaf59df8ad5df51eaf44994866595912813
EliaStocco <eliastocco@gmail.com>
2024-06-05 12:11:23 +0200

    changed way to create a MACE model and to compute derivatives

--- eslib/classes/mace_model.py
+++ eslib/classes/mace_model.py
@@ -1,5 +1,5 @@
 from ase import Atoms
-from typing import List, Dict
+from typing import List, Dict, Any, Optional
 from dataclasses import dataclass, field
 import numpy as np
 from mace.tools import torch_geometric, torch_tools
@@ -11,73 +11,88 @@ from eslib.tools import add_info_array, reshape_info_array
 import torch
 import json
 import os
+from mace.modules.models.general import MACEBaseModel
 
 @dataclass
 class MACEModel(pickleIO):
     default_dtype: str
     device: str
-    model: str
-    model_type: str
+    model_path: str
+    # model_type: str
     batch_size: int
     charges_key: str
-    _initialized:bool = field(init=False)
-    _attributes_dict: Dict[str, str] = field(init=False)
-    _folder: str = field(init=False)
+    dR:bool
+    to_diff_props:List
+    rename_props:Dict[str,Any]
 
-    def to_pickle(self,*argv,**kwargs):
-        # self._initialized = False
-        # if not reinit:
-        # try:
-        #     super().to_pickle(*argv,**kwargs)
-        # except:
-        self._initialized = False
-        super().to_pickle(*argv,**kwargs)
-        # else:
-        #     self._initialized = False
-        #     super().to_pickle(*argv,**kwargs)
+    # to_diff_props=["dipole"]
+    # rename_dR={"dipole_dR":"BEC"}
 
-    def __post_init__(self):
-        self._attributes_dict = self.to_dict()
-        self._initialized = False
-        self._folder = os.getcwd()
-        self.initialize()
+    # _initialized:bool = field(init=False)
+    # _attributes_dict: Dict[str, str] = field(init=False)
+    # _folder: str = field(init=False)
 
-    def to_dict(self) -> Dict[str, str]:
-        # Get all fields of the dataclass
-        fields = self.__dataclass_fields__
+    # def to_pickle(self,*argv,**kwargs):
+    #     # self._initialized = False
+    #     # if not reinit:
+    #     # try:
+    #     #     super().to_pickle(*argv,**kwargs)
+    #     # except:
+    #     self._initialized = False
+    #     super().to_pickle(*argv,**kwargs)
+    #     # else:
+    #     #     self._initialized = False
+    #     #     super().to_pickle(*argv,**kwargs)
 
-        # Store attribute values in a dictionary for initialized fields only
-        attributes_dict = {field_name: getattr(self, field_name) 
-                           for field_name in fields 
-                           if field_name in self.__dict__}
+    # def __post_init__(self):
+    #     # self._attributes_dict = self.to_dict()
+    #     # self._initialized = False
+    #     # self._folder = os.getcwd()
+    #     self.initialize()
+
+    # def to_dict(self) -> Dict[str, str]:
+    #     # Get all fields of the dataclass
+    #     fields = self.__dataclass_fields__
+
+    #     # Store attribute values in a dictionary for initialized fields only
+    #     attributes_dict = {field_name: getattr(self, field_name) 
+    #                        for field_name in fields 
+    #                        if field_name in self.__dict__}
         
-        return attributes_dict
+    #     return attributes_dict
     
-    def to_json(self,file):
-        with open(file,"w") as ffile:
-            json.dump(obj=self._attributes_dict,fp=ffile,indent=4)
+    # def to_json(self,file):
+    #     with open(file,"w") as ffile:
+    #         json.dump(obj=self._attributes_dict,fp=ffile,indent=4)
 
-    def initialize(self):     
-        if not self._initialized:   
-            torch_tools.set_default_dtype(self.default_dtype)
-            self.device = torch_tools.init_device([self.device])
-            try :
-                model_path = self.model
-                self.network = get_model(model_path=model_path,
-                                    model_type=self.model_type,
-                                    device=self.device)
-            except:
-                model_path = "{:s}/{:s}".format(self._folder,self.model)
-                self.network = get_model(model_path=model_path,
-                                    model_type=self.model_type,
-                                    device=self.device)
-            self.network = self.network.to(self.device)  # shouldn't be necessary but seems to help with CUDA problems
-            for param in self.network.parameters():
-                param.requires_grad = False
-            self._initialized = True
+    def __post_init__(self):     
+        # if not self._initialized:   
+        torch_tools.set_default_dtype(self.default_dtype)
+        self.device = torch_tools.init_device([self.device])
+        # try :
+        # model_path = self.model_path
+        self.network :MACEBaseModel = torch.load(f=self.model_path, map_location=self.device)
+        # self.network = get_model(model_path=model_path,
+        #                     model_type=self.model_type,
+        #                     device=self.device)
+        # except:
+        #     model_path = "{:s}/{:s}".format(self._folder,self.model)
+        #     self.network = get_model(model_path=model_path,
+        #                         model_type=self.model_type,
+        #                         device=self.device)
+        self.network = self.network.to(self.device)  # shouldn't be necessary but seems to help with CUDA problems
+        for param in self.network.parameters():
+            param.requires_grad = False
+        # self._initialized = True
+        if self.dR:
+            new_prop = get_d_prop_dR(self.to_diff_props,type(self.network),self.rename_props)
+            self.network.implemented_properties = {**self.network.implemented_properties,**new_prop}
+
+        self.network.set_prop()
 
     def compute(self,traj:List[Atoms],prefix:str="",raw:bool=False,**argv):
-        self.initialize()
+        # self.initialize()
+        torch_tools.set_default_dtype(self.default_dtype)
         data_loader:torch_geometric.dataloader.DataLoader = \
             make_dataloader(atoms_list=traj,
                             model=self.network,
@@ -86,9 +101,11 @@ class MACEModel(pickleIO):
         warnings = []
         outputs:Dict[str,np.ndarray] = dict()
         for batch in data_loader:
-            batch = batch.to(self.device)
-            results:Dict[str,torch.Tensor] = self.network(batch.to_dict(), compute_stress=False)
-
+            batch:torch_geometric.batch.Batch = batch.to(self.device)
+            data:Dict[str,torch.Tensor] = batch.to_dict()
+            results:Dict[str,torch.Tensor] = self.network(data, compute_stress=False)
+            if self.dR: 
+                results = add_derivatives(self,results,data)
             for k in results.keys():
                 if k not in self.network.implemented_properties:
                     if k not in warnings:
@@ -114,12 +131,13 @@ class MACEModel(pickleIO):
     
     def summary(self, string="\t"):
         args = {
-            "path": self.model,
-            "type": self.model_type,
+            "path": self.model_path,
+            # "type": self.model_type,
             "device": self.device,            
             "batch size": self.batch_size,
             "charges key": self.charges_key,
             "dtype" : self.default_dtype,
+            "derivatives" : self.dR,
             "properties": list(self.network.implemented_properties.keys())
         }
 
@@ -130,4 +148,72 @@ class MACEModel(pickleIO):
             # Align the output based on the length of the longest key
             print("{:s}{:<{width}}: {}".format(string, k, v, width=max_key_length))
 
-        
+def compute_dielectric_gradients(
+    dielectric: torch.Tensor,
+    positions: torch.Tensor,
+) -> torch.Tensor:
+    d_dielectric_dr = []
+    for i in range(dielectric.shape[-1]):
+        grad_outputs: List[Optional[torch.Tensor]] = [
+            torch.ones((dielectric.shape[0], 1)).to(dielectric.device)
+        ]
+        gradient = torch.autograd.grad(
+            outputs=[dielectric[:, i].unsqueeze(-1)],  # [n_graphs, 3], [n_graphs, 9]
+            inputs=[positions],  # [n_nodes, 3]
+            grad_outputs=grad_outputs,
+            retain_graph=True,  # Make sure the graph is not destroyed during training
+            create_graph=True,  # Create graph for second derivative
+            allow_unused=True,  # For complete dissociation turn to true
+        )
+        d_dielectric_dr.append(gradient[0])
+    d_dielectric_dr = torch.stack(d_dielectric_dr, dim=1)
+    if gradient[0] is None:
+        return torch.zeros((positions.shape[0], dielectric.shape[-1], 3))
+    return d_dielectric_dr
+
+def add_derivatives(model:MACEModel,output:Dict[str,torch.Tensor],data:Dict[str,torch.Tensor])->Dict[str,torch.Tensor]:
+    for prop in model.to_diff_props:
+        array = compute_dielectric_gradients(
+            dielectric=output[prop],
+            positions=data["positions"],
+        )
+        name = "{:s}_dR".format(prop)
+        name = name if name not in model.rename_props else model.rename_props[name]
+
+        # # Determine the number of axes: (atom, positions coord, output coord)
+        # num_axes = tmp.dim()
+
+        # # Permute the axis order
+        # permuted_order = list(range(num_axes))
+        # permuted_order = [permuted_order[-1]] + permuted_order[:-1]  # Move last axis to the front
+        # permuted_tensor = tmp.permute(permuted_order)
+
+        # (output coord, atom, positions coord)
+        output[name] = array
+        if output[prop].shape[1] == 3:
+            for n,i in enumerate(["x","y","z"]):
+                output["{:s}{:s}".format(name,i)] = array[:,:,n]
+
+    return output
+
+def add_natoms(info):
+    shape = info[1]
+    if type(shape) == int:
+        return (info[0], ("natoms",3,shape))
+    else:
+        return (info[0], ("natoms",3,) + shape)
+
+def get_d_prop_dR(props:List,basecls:MACEBaseModel,rename:Dict[str,str])->Dict[str,Any]:
+    # der = [None]*len(props)
+    der = {}
+    ip = basecls.implemented_properties
+    for prop in props:
+        if prop not in ip:
+            raise ValueError("'{:s}' is not an implemented property of the parent class {}.".format(prop,basecls))
+        name ="{:s}_dR".format(prop)
+        name = name if name not in rename else rename[name]
+        der[name] = add_natoms(ip[prop])
+        if ip[prop][1] == 3:
+            for n,i in enumerate(["x","y","z"]):
+                der["{:s}{:s}".format(name,i)] = (ip[prop][0], ("natoms",3))
+    return der
--- eslib/scripts/nn/create-mace-model.py
+++ eslib/scripts/nn/create-mace-model.py
@@ -2,6 +2,8 @@
 from eslib.formatting import esfmt, warning
 from eslib.classes.mace_model import MACEModel
 from eslib.functions import args_to_dict
+from eslib.input import slist, str2bool
+import json
 
 #---------------------------------------#
 description = "Allocate a MACE model and save it to a pickle file for later easier access."
@@ -11,24 +13,39 @@ def prepare_args(description):
     import argparse
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
-    parser.add_argument("-m" , "--model"        , **argv, type=str, required=True , help="*.pth file with the MACE model")
-    parser.add_argument("-mt", "--model_type"   , **argv, type=str, required=True , help="model type")
+    parser.add_argument("-i" , "--input"        , **argv, type=str, required=False, help="JSON file with all the following parameters default: %(default)s)", default=None)
+    parser.add_argument("-m" , "--model_path"   , **argv, type=str, required=True , help="*.pth file with the MACE model")
+    # parser.add_argument("-mt", "--model_type"   , **argv, type=str, required=True , help="model type")
     parser.add_argument("-d" , "--device"       , **argv, type=str, required=False, help="device (default: %(default)s)", choices=["cpu", "cuda"], default="cpu")
     parser.add_argument("-dt", "--default_dtype", **argv, type=str, required=False, help="default dtype (default: %(default)s)", choices=["float32", "float64"], default="float64")
     parser.add_argument("-bs", "--batch_size"   , **argv, type=int, required=False, help="batch size (default: %(default)s)", default=64)
     parser.add_argument("-ck", "--charges_key"  , **argv, type=str, required=False, help="key of atomic charges (default: %(default)s)", default="Qs")
+    parser.add_argument("-dR", "--dR"           , **argv, type=str2bool, required=False, help="whether to compute spatial derivatives (default: %(default)s)", default=True)
+    parser.add_argument("-dp", "--to_diff_props"  , **argv, type=slist, required=False, help="properties to be differentiated (default: %(default)s)", default=["dipole"])
+    parser.add_argument("-rp", "--rename_props"  , **argv, type=dict, required=False, help="properties to be renamed (default: %(default)s)", default={"dipole_dR":"BEC"})
     parser.add_argument("-o" , "--output"       , **argv, type=str, required=True , help="*.pickle output file with the MACE model")
-    parser.add_argument("-jo", "--json_output"  , **argv, type=str, required=False, help="JSON output file with all the previous instructions to initialize the MACE model (default: %(default)s)",default=None)
+    # dR:bool
+    # to_diff_props:List
+    # rename_dR:Dict[str,Any]
+    # parser.add_argument("-jo", "--json_output"  , **argv, type=str, required=False, help="JSON output file with all the previous instructions to initialize the MACE model (default: %(default)s)",default=None)
     return parser
 
 #---------------------------------------#
 @esfmt(prepare_args,description)
 def main(args):
 
+    if args.input is not None:
+        print("\tReading parameters from file '{:s}' ... ".format(args.input),end="")
+        with open(args.input, 'r') as f:
+            kwargs = json.load(f)
+        print("done")
+    else:
+        kwargs = args_to_dict(args)
+    
+    if "input" in kwargs: del kwargs["input"]
+    if "output" in kwargs: del kwargs["output"]
+
     print("\n\tAllocating the MACE model ... ",end="")
-    kwargs = args_to_dict(args)
-    del kwargs["output"]
-    del kwargs["json_output"]
     model = MACEModel(**kwargs)
     print("done")
 
@@ -37,19 +54,19 @@ def main(args):
     model.summary(string="\t\t")
 
     #------------------#
-    try:
-        print("\n\tSaving the MACE model to file '{:s}' ... ".format(args.output),end="")
-        model.to_pickle(args.output)
-        print("done") 
-    except:
-        print("\n\t{:s}: a problem occurred by saving the model to file using `pickle`.".format(warning))
-        if args.json_output is None:
-            print("It's recommended to specify -jo,--json_output.")
+    # try:
+    print("\n\tSaving the MACE model to file '{:s}' ... ".format(args.output),end="")
+    model.to_pickle(args.output)
+    print("done") 
+    # except:
+    #     print("\n\t{:s}: a problem occurred by saving the model to file using `pickle`.".format(warning))
+    #     if args.json_output is None:
+    #         print("It's recommended to specify -jo,--json_output.")
 
-    if args.json_output is not None:
-        print("\n\tSaving the MACE model input arguments to file '{:s}' ... ".format(args.json_output),end="")
-        model.to_json(args.json_output)
-        print("done")
+    # if args.json_output is not None:
+    #     print("\n\tSaving the MACE model input arguments to file '{:s}' ... ".format(args.json_output),end="")
+    #     model.to_json(args.json_output)
+    #     print("done")
 
 #---------------------------------------#
 if __name__ == "__main__":
