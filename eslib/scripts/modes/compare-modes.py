6b12e038e8d05dbbf63be05b734dbee2ae602335
EliaStocco <eliastocco@gmail.com>
2024-06-06 15:33:48 +0200

    improved plotd

--- eslib/scripts/modes/compare-modes.py
+++ eslib/scripts/modes/compare-modes.py
@@ -5,11 +5,18 @@ from eslib.classes.physical_tensor import PhysicalTensor, corrected_sqrt
 from eslib.output import output_folder
 import numpy as np
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from eslib.plot import square_plot, plot_bisector, hzero
 from icecream import ic
 from eslib.tools import convert
+
+# Set the backend to QtAgg
+matplotlib.use('QtAgg')
+
+# Enable interactive mode
+plt.ion()
 
 #---------------------------------------#
 # Description of the script's purpose
@@ -44,14 +51,14 @@ def main(args):
     output_folder(args.output)
 
     #---------------------------------------#
-    Amodes = A.mode.copy()
-    Bmodes = B.mode.copy()
+    Amodes = A.mode.copy().real
+    Bmodes = B.mode.copy().real
 
     Amodes = PhysicalTensor(Amodes).rename({'mode': 'mode-a'})  
     Bmodes = PhysicalTensor(Bmodes).rename({'mode': 'mode-b'})   
-    prod =  Amodes.cdot(Bmodes,"dof").to_numpy()[args.dof:,args.dof:]
+    prod =  np.abs(Amodes.cdot(Bmodes,"dof").to_numpy()[args.dof:,args.dof:])
     cos = np.arccos(prod)*180/np.pi
-    cos = np.asarray([ 90 - abs(c - 90) if c > 90 else c for c in cos.flatten() ]).reshape(cos.shape)
+    cos = np.abs(np.asarray([ 90 - abs(c - 90) if c > 90 else c for c in cos.flatten() ]).reshape(cos.shape))
 
     if np.any(np.abs(prod)>1):
         print("\t{:s}: values with modulus greater than 1.".format(warning))
@@ -68,8 +75,13 @@ def main(args):
     cmap_name = 'custom_cmap'
     cmap = LinearSegmentedColormap.from_list(cmap_name, colors)
 
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(prod, cmap=cmap, annot=False)
+    fig, ax = plt.subplots(1,1,figsize=(6,6))
+    plt.title("Scalar product between normal modes: $|{\\rm N}_i^A\\cdot {\\rm N}_j^B|$")
+    sns.heatmap(prod, cmap='viridis', annot=False)
+    # im = ax.imshow(prod, cmap='viridis')
+    # ax.set_aspect('equal')
+    # # square_plot(ax)
+    # plt.colorbar(im, ax=ax, shrink=1)
     plt.savefig("{:s}/prod.pdf".format(args.output))
 
     #
@@ -77,8 +89,9 @@ def main(args):
     cmap_name = 'custom_cmap'
     cmap = LinearSegmentedColormap.from_list(cmap_name, colors)
 
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(cos, cmap=cmap, annot=False)
+    plt.figure(figsize=(6, 6))
+    plt.title("Angle [deg] between normal modes: arccos $\\left({\\rm N}_i^A\\cdot {\\rm N}_j^B\\right)$")
+    sns.heatmap(cos, cmap='viridis', annot=False)
     plt.savefig("{:s}/cos.pdf".format(args.output))
 
     #---------------------------------------#
@@ -87,10 +100,14 @@ def main(args):
     array1 = corrected_sqrt(A.get("eigval"))*factor
     array2 = corrected_sqrt(B.get("eigval"))*factor
 
+    ii = np.logical_and(array1 > 0,array2 > 0 )
+    array1 = array1[ii]
+    array2 = array2[ii]
+
     # Plotting the correlation heatmap
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True,figsize=(6, 6))
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True,figsize=(6, 10))
     ax1.scatter(array1,array2,color="blue",s=5)
-    ax1.grid()
+    # ax1.grid()
     ax1.set_xlabel("A freq. [THz]")
     ax1.set_ylabel("B freq. [THz]")
     square_plot(ax1)
@@ -99,20 +116,26 @@ def main(args):
     width = (array1.max() - array1.min())/ (5*len(array1))
     y = np.abs(array1-array2)
     ax2.bar(array1,y,width=width,color="blue")
-    ax2.grid()
-    ax2.set_xlabel("A freq. [THz]")
+    # ax2.grid()
     ax2.set_ylabel("$\\Delta$ freq. [THz]",color="blue")
     ax2.set_ylim(0,y.max()*1.1)
     hzero(ax2)
 
     # Create a twin axis for ax2
-    ax3 = ax2.twinx()
+    # ax3 = ax2.twinx()
     ax3.set_ylabel("perc. err.",color="red")  # Label for the right y-axis
     y = 100 * np.abs((array1-array2)/array1)
     ax3.bar(array1,y,width=width,color="red")
-
+    hzero(ax3)
     ax3.set_ylim(0,y.max()*1.1)
     # hzero(ax3)
+    ax3.set_xlabel("A freq. [THz]")
+
+    # xlim = ax3.get_xlim()
+    
+
+    for ax in [ax1, ax2, ax3]:
+        ax.grid()
 
     plt.tight_layout()
     plt.savefig("{:s}/corr-eigval.pdf".format(args.output))
