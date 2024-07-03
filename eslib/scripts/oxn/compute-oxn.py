0b849c7f077f05bee44bab65ea6272359e3f4de1
EliaStocco <eliastocco@gmail.com>
2024-07-03 22:08:46 +0200

    small changes

--- eslib/scripts/oxn/compute-oxn.py
+++ eslib/scripts/oxn/compute-oxn.py
@@ -20,7 +20,7 @@ def prepare_args(description):
     argv = {"metavar" : "\b",}
     parser.add_argument("-i" , "--input"       , **argv, required=True , type=str, help="input file [extxyz]")
     parser.add_argument("-if", "--input_format", **argv, required=False, type=str, help="input file format (default: %(default)s)" , default=None)
-    parser.add_argument("-f" , "--fix"     , **argv, required=False, type=str2bool, help="fix branch", default=True)
+    parser.add_argument("-f" , "--fix"     , **argv, required=False, type=str2bool, help="fix branch", default=False)
     parser.add_argument("-k" , "--keyword"     , **argv, required=False, type=str, help="keyword the dipoles (default: %(default)s)", default="dipole")
     parser.add_argument("-o", "--output", **argv, required=False, type=str, help="output file (default: %(default)s)" , default=None)
     return parser
