a6f5b51a720c467b0c13c2a838a9a54d0225c40b
EliaStocco <eliastocco@gmail.com>
2024-07-04 22:24:28 +0200

    better formatting

--- eslib/scripts/drivers/macelia.py
+++ eslib/scripts/drivers/macelia.py
@@ -71,7 +71,6 @@ def main(args):
         from eslib.classes.mace_model import MACEModel
         print("\tLoading eslib MACE model from file '{:s}' ... ".format(args.model), end="")
         calculator = MACEModel.from_pickle(args.model)
-        calculator.summary()
         
     elif args.model_type == "MACE":
         print("\tLoading a MACECalculator based on the model that you provided ... ", end="")
@@ -82,6 +81,11 @@ def main(args):
                                     model_type=args.model_type,\
                                     **kwargv)         
     print("done")
+
+    try:
+        calculator.summary()
+    except:
+        pass
 
     atoms.calc = calculator # atoms.set_calculator(calculator)
 
