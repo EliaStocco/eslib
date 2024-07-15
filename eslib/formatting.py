6ea80ea799efd476e4eca9e2139074e5cf3b636e
EliaStocco <eliastocco@gmail.com>
2024-07-15 15:55:01 +0200

    small changes

--- eslib/formatting.py
+++ eslib/formatting.py
@@ -143,9 +143,9 @@ def esfmt(prepare_parser:callable=None, description:str=None,documentation:str=N
         # seconds = int(elapsed_seconds % 60)
         line(end="###")
         print("end date: {:s}".format(end_date))
-        print("end time: {:s}".format(end_time))
+        print("end time: {:s}s ".format(end_time))
         print("elapsed time: {:s}".format(format_seconds_to_hhmmss(elapsed_seconds)))
-        print("elapsed seconds: {:d}".format(elapsed_seconds))
+        print("elapsed seconds: {:d}s".format(elapsed_seconds))
         line(end="###\n")
 
     def wrapper(main: callable):
@@ -183,52 +183,3 @@ def esfmt(prepare_parser:callable=None, description:str=None,documentation:str=N
         return wrapped_main
 
     return wrapper
-
-def matrix2str(matrix,
-                 row_names=["x","y","z"], 
-                 col_names=["1","2","3"], 
-                 exp=False,
-                 width=8, 
-                 digits=2,
-                 prefix="\t",
-                 cols_align="^",
-                 num_align=">"):
-    """
-    Print a formatted 3x3 matrix with customizable alignment.
-
-    Parameters:
-    - matrix: The 2D matrix to be printed.
-    - row_names: List of row names. Default is ["x", "y", "z"].
-    - col_names: List of column names. Default is ["1", "2", "3"].
-    - exp: If True, format matrix elements in exponential notation; otherwise, use fixed-point notation.
-    - width: Width of each matrix element.
-    - digits: Number of digits after the decimal point.
-    - prefix: Prefix string for each line.
-    - cols_align: Alignment for column names. Use '<' for left, '^' for center, and '>' for right alignment.
-    - num_align: Alignment for numeric values. Use '<' for left, '^' for center, and '>' for right alignment.
-
-    Example:
-    print_matrix(matrix, row_names=["A", "B", "C"], col_names=["X", "Y", "Z"], exp=True, width=10, digits=3, prefix="\t", cols_align="^", num_align=">")
-    """
-    # Determine the format string for each element in the matrix
-    exp = "e" if exp else "f" 
-    format_str = f'{{:{num_align}{width}.{digits}{exp}}}'
-    format_str_all = [format_str]*matrix.shape[1]
-    hello = f'{{:s}}| {{:s}} |' + f'{{:s}}'*matrix.shape[1] + f' |\n'
-    # Find the maximum length of row names for formatting
-    L = max([ len(i) for i in row_names ])
-    row_str = f'{{:>{L}s}}'
-    # Construct the header with column names
-    text = '{:s}| ' + row_str + ' |' + (f'{{:{cols_align}{width}s}}')*matrix.shape[1] + ' |\n'
-    text = text.format(prefix,"",*list(col_names))
-    division = prefix + "|" + "-"*(len(text) - len(prefix) - 3) + "|\n"
-    text = division + text + division 
-    # Add row entries to the text
-    for i, row in enumerate(matrix):
-        name_str = row_str.format(row_names[i]) if row_names is not None else ""
-        formatted_row = hello.format(prefix,name_str,*format_str_all)
-        line = formatted_row.format(*list(row))
-        text += line
-    # Add a final divider and print the formatted matrix
-    text += division
-    return text
