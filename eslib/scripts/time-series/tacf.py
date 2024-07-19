ea3ae1a5f74a5c0fc2b751925e9cf266e69e1c20
EliaStocco <eliastocco@gmail.com>
2024-07-19 09:40:47 +0200

    fixed script to compute Time AutoCorrelation Function

--- eslib/scripts/time-series/tacf.py
+++ eslib/scripts/time-series/tacf.py
@@ -6,6 +6,8 @@ from eslib.mathematics import tacf, reshape_into_blocks
 import matplotlib.pyplot as plt 
 from eslib.plot import hzero
 from scipy.optimize import curve_fit
+from eslib.classes.physical_tensor import PhysicalTensor
+from eslib.input import str2bool
 
 #---------------------------------------#
 # Description of the script's purpose
@@ -17,11 +19,16 @@ def prepare_args(description):
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
     parser.add_argument("-i" , "--input"  , **argv, required=True , type=str, help="txt/npy input file")
-    parser.add_argument("-b" , "--blocks" , **argv, required=False, type=int, help="number of blocks (default: %(default)s)", default=10)
+    parser.add_argument("-dt" , "--time_step"  , **argv, required=False , type=float, help="time step [fs] (default: %(default)s)", default=1)
+    parser.add_argument("-d" , "--derivative"  , **argv, required=False , type=str2bool, help="compute derivative of the input data (default: %(default)s)", default=False)
+    parser.add_argument("-b" , "--blocks" , **argv, required=False, type=int, help="number of blocks (default: %(default)s)", default=0)
     parser.add_argument("-m" , "--method" , **argv, required=False, type=str, help="method (default: %(default)s)", default='class', choices=['class','function'])
     parser.add_argument("-o" , "--output" , **argv, required=False, type=str, help="txt/npy output file (default: %(default)s)", default='tacf.npy')
     parser.add_argument("-p" , "--plot"   , **argv, required=False, type=str, help="output file for the plot (default: %(default)s)", default='tacf.pdf')
-
+    # parser.add_argument("-at" , "--axis_traj"   , **argv, required=False, type=int, help="axis corresponding to the trajectory (default: %(default)s)", default=0)
+    parser.add_argument("-ac" , "--axis_corr"   , **argv, required=False, type=int, help="axis along compute autocorrelation (default: %(default)s)", default=0)
+    parser.add_argument("-am" , "--axis_mean"   , **argv, required=False, type=int, help="axis along compute mean (default: %(default)s)", default=None)
+    parser.add_argument("-rm" , "--remove_mean"   , **argv, required=False, type=str2bool, help="whether to remove the e mean (default: %(default)s)", default=False)
     return parser
 
 #---------------------------------------#
@@ -29,24 +36,32 @@ def prepare_args(description):
 def main(args):
 
     #------------------#
-    print("\tReading the input array from file '{:s}' ... ".format(args.input), end="")
+    print("\n\tReading the input array from file '{:s}' ... ".format(args.input), end="")
     args.input = str(args.input)
-    if args.input.endswith("npy"):
-        data = np.load(args.input)
-    else:
-        data = np.loadtxt(args.input)
+    data:np.ndarray = PhysicalTensor.from_file(file=args.input).to_data()#[None,:,:]
     print("done")
-    print("\tdata shape: :",data.shape)
+    print("\tdata shape: ",data.shape)
 
     #------------------#
-    print("\tn. of blocks: ", args.blocks)
-    data = reshape_into_blocks(data,args.blocks).T
-    print("\tdata shape: :",data.shape)
+    if args.blocks > 0 :
+        print("\n\tN. of blocks: ", args.blocks)
+        print("\tBuilding blocks ... ",end="")
+        data = reshape_into_blocks(data,args.blocks)# .T
+        print("done")
+        print("\tdata shape: ",data.shape)
 
     #------------------#
-    print("\tremoving mean ... ",end="")
-    data -= data.mean(axis=0)
-    print("done")
+    if args.derivative:
+        print("\n\tComputing the derivative ... ",end="")
+        data = np.gradient(data,axis=args.axis_corr)/args.time_step
+        print("done")
+        print("\tdata shape: ",data.shape)
+
+    #------------------#
+    if args.remove_mean:
+        print("\n\tRemoving mean ... ",end="")
+        data -= np.mean(data,axis=args.axis_corr,keepdims=True)
+        print("done")
 
     #------------------#
     print("\n\tComputing the autocorrelation function ... ", end="")
@@ -55,25 +70,44 @@ def main(args):
     else:
         from eslib.classes.tcf import TimeAutoCorrelation
         obj = TimeAutoCorrelation(data)
-        autocorr = obj.tcf()
+        autocorr = obj.tcf(axis=args.axis_corr)
     print("done")
-    print("\tautocorr shape: :",autocorr.shape)
+    print("\tautocorr shape: ",autocorr.shape)
 
     #------------------#
-    print("\n\tSaving TACF to file '{:s}' ... ".format(args.output), end="")
-    if args.output.endswith("npy"):
-        np.save(args.output,autocorr)
-    else:
-        np.savetxt(args.output,autocorr)
+    if args.axis_mean is not None:
+        print("\n\tComputing the mean along the axis {:d} ... ".format(args.axis_mean),end="")
+        autocorr = np.mean(autocorr,axis=args.axis_mean)
+        print("done")
+        print("\tautocorr shape: ",autocorr.shape)
+
+    #------------------#
+    print("\n\tConverting autocorr into PhysicalTensor ... ", end="")
+    autocorr = PhysicalTensor(autocorr)
     print("done")
 
     #------------------#
-    print("\n\tFitting the autocorrelation function with an exponential ... ", end="")
+    print("\tSaving TACF to file '{:s}' ... ".format(args.output), end="")
+    autocorr.to_file(file=args.output)
+    print("done")
 
-    mean = autocorr.mean(axis=1)
-    std = autocorr.std(axis=1)
-    x = np.arange(len(mean))
+    #------------------#
 
+    if autocorr.ndim == 1:
+        print("\tReshaping data for plotting  ... ", end="")
+        autocorr = np.atleast_2d(autocorr)# .T
+        print("done")
+        print("\tdata shape: ",data.shape)
+
+    print("\n\tFitting the autocorrelation function with an exponential ... ")
+
+    mean = autocorr.mean(axis=0)
+    std = autocorr.std(axis=0)
+    x = np.arange(autocorr.shape[1])*args.time_step
+    print("\t\t   x shape: ",x.shape)
+    print("\t\tmean shape: ",mean.shape)
+    print("\t\t std shape: ",std.shape)
+    
     # Define the exponential function to fit
     def exponential(x,tau):
         return np.exp(-x/tau)
@@ -85,17 +119,17 @@ def main(args):
                            sigma=std)
 
     yfit = exponential(x, *popt)
-    print("done")
-    print("\ttau: {:.0f}".format(popt[0]))
-
+    # print("\tdone")
+    print("\t\ttau: {:.0f}".format(popt[0]))
 
     #------------------#
     if args.plot is not None:
         print("\n\tProducing autocorrelation plot ... ", end="")
         fig, axes = plt.subplots(2, 1, figsize=(10, 8),sharex=True)
 
-        for i in range(autocorr.shape[1]):
-            axes[0].plot(autocorr[:, i], label=f'{i+1}')
+        for i in range(autocorr.shape[0]):
+            y = autocorr[i]
+            axes[0].plot(x,y, label=f'{i+1}')
 
         axes[1].plot(x[1:],mean[1:],color="blue",label='$\\mu$')
         ylow,yhigh = mean - std, mean + std
@@ -110,16 +144,16 @@ def main(args):
             ax.set_xlim(1,x.max())
             hzero(ax)
             # ax.set_xlim(1,x.max())
-            ax.set_xscale("log")
+            # ax.set_xscale("log")
         
         axes[1].legend(loc="upper right",facecolor='white', framealpha=1,edgecolor="black")
         axes[1].set_xlabel('x')
 
         # Add text box with the value of a variable
-        textbox_text = "$\\tau\\approx{:.0f}$".format(popt[0]) 
+        # textbox_text = "$\\tau\\approx{:.0f}$".format(popt[0]) 
         # plt.text(0.1, 0.05, textbox_text,  ha='center', va='top', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
-        plt.annotate(textbox_text, xy=(0.5, 0.95), xycoords='axes fraction', fontsize=10,
-                ha='center', va='top', bbox=dict(facecolor='white', alpha=1,edgecolor="black"))
+        # plt.annotate(textbox_text, xy=(0.5, 0.95), xycoords='axes fraction', fontsize=10,
+        #         ha='center', va='top', bbox=dict(facecolor='white', alpha=1,edgecolor="black"))
         plt.tight_layout()
         print("done")
 
