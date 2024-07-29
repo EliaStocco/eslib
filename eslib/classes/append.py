b566a9d0c449e07c9acc9d95f6e9a8f5c807d7c8
EliaStocco <eliastocco@gmail.com>
2024-07-29 10:51:48 +0200

    optimized code

--- eslib/classes/append.py
+++ eslib/classes/append.py
@@ -1,8 +1,9 @@
+from git import Union
 import numpy as np
 from typing import TypeVar
 
 
-T = TypeVar('T')
+T = TypeVar('T',bound="AppendableArray")
 
 
 class AppendableArray:
@@ -11,7 +12,7 @@ class AppendableArray:
     If the array is full, it doubles its size.
     """
 
-    def __init__(self, size: int = 100000) -> None:
+    def __init__(self:T, size: int = 100000) -> None:
         """
         Initialize a new AppendableArray.
 
@@ -23,20 +24,33 @@ class AppendableArray:
         self._max_size: int = size
         self._n_update: int = 0
 
-    def append(self, x: T) -> None:
+    def append(self:T, x: Union[np.ndarray,float]) -> None:
         """
         Append an array to the array.
 
         Args:
-            x (T): The array to append.
+            x (Union[np.ndarray, float]): The array to append. It can be a single float or a numpy array.
 
         """
-        if self._size + len(x) > self._max_size:
-            self._expand()
-        self._arr[self._size:self._size + len(x)] = x
-        self._size += len(x)
+        # Check if x is a single float
+        if np.ndim(x) == 0:
+            # If the array is full, double its size
+            if self._size + 1 > self._max_size:
+                self._expand()
+            # Append the float to the array
+            self._arr[self._size] = x
+            # Increment the size counter
+            self._size += 1
+        else:  # x is an array
+            # If the array is not large enough to hold x, double its size
+            if self._size + len(x) > self._max_size:
+                self._expand()
+            # Append the array to the array
+            self._arr[self._size:self._size + len(x)] = x
+            # Increment the size counter
+            self._size += len(x)
 
-    def _expand(self) -> None:
+    def _expand(self:T) -> None:
         """
         Double the size of the array.
         """
@@ -47,7 +61,7 @@ class AppendableArray:
         self._max_size = new_size
         self._n_update += 1
 
-    def finalize(self) -> T:
+    def finalize(self:T) -> np.ndarray:
         """
         Return the final array.
 
--- eslib/scripts/dipole/extras2dipole.py
+++ eslib/scripts/dipole/extras2dipole.py
@@ -5,12 +5,13 @@ from eslib.input import str2bool
 import numpy as np
 import re
 from eslib.classes.physical_tensor import PhysicalTensor
+from eslib.classes.append import AppendableArray
 
-#---------------------------------------#
+# ---------------------------------------#
 # Description of the script's purpose
 description = "Read the dipoles from a i-PI extras file and save them in a txt file."
 
-#---------------------------------------#
+# ---------------------------------------#
 def prepare_args(description):
     import argparse
     parser = argparse.ArgumentParser(description=description)
@@ -21,38 +22,38 @@ def prepare_args(description):
     parser.add_argument("-o" , "--output", **argv, required=False, type=str, help="txt output file with dipoles (default: %(default)s)", default='dipoles.txt')
     return parser# .parse_args()
 
-#---------------------------------------#
+# ---------------------------------------#
 @esfmt(prepare_args,description)
 def main(args):
 
-    #------------------#
+    step = re.compile(r"Step:\s+(\d+)")
+    dipoles = AppendableArray()
+    steps = AppendableArray()
+    # ------------------#
     print("\tReading dipoles from file '{:s}' ... ".format(args.input), end="")
     # Open the input file for reading
     with open(args.input, 'r') as f:
-        lines = f.readlines()
-
-    # Initialize an empty list to store dipole values
-    dipoles = []
-    steps = []
-    step = re.compile(r"Step:\s+(\d+)")
-    # strings = [ step.search(comment).group(1) for comment in comments ]
-    #         steps = np.asarray([int(i) for i in strings],dtype=int)
-
-    # Iterate through each line in the file
-    for line in lines:
-        test = step.search(line)
-        if test is not None:
-            steps.append(int(test.group(1)))
-        # Check if the line contains dipole data
-        if '\"{:s}\"'.format(args.keyword) in line:
-            # Extract the dipole values from the line
-            json_data = json.loads(line.replace("\n","").replace('\x00',""))
-            dipole_data = json_data[args.keyword] #json.loads(line.strip().split('"dipole":')[1][:-1])
-            # Append the dipole values to the list
-            dipoles.append(dipole_data)
+        line = f.readline()
+        # dipoles = []
+        # steps = []
+        while line:
+            test = step.search(line)
+            if test is not None:
+                steps.append(int(test.group(1)))
+            # Check if the line contains dipole data
+            if '\"{:s}\"'.format(args.keyword) in line:
+                # Extract the dipole values from the line
+                json_data = json.loads(line.replace("\n","").replace('\x00',""))
+                dipole_data = json_data[args.keyword]
+                # Append the dipole values to the list
+                dipoles.append(dipole_data)
+            line = f.readline()
     print("done")
 
-    #------------------#
+    dipoles = dipoles.finalize()
+    steps = steps.finalize()
+
+    # ------------------#
     steps= np.asarray(steps)
     test, indices = np.unique(steps, return_index=True)
     if steps.shape != test.shape and not args.remove_replicas:
@@ -60,21 +61,13 @@ def main(args):
     if args.remove_replicas:
         dipoles = [dipoles[index] for index in indices]
 
-    #------------------#
+    # ------------------#
     print("\tConverting dipoles to np.array: ... ", end="")
     dipoles = np.asarray(dipoles).reshape((-1,3))
     print("done")
     print("\tdipoles shape: ",dipoles.shape, end="")
 
-    #------------------#
-    # print("\n\tWriting dipoles to file '{:s}' ... ".format(args.output), end="")
-    # try:
-    #     np.savetxt(args.output,dipoles,fmt=float_format)
-    #     print("done")
-    # except Exception as e:
-    #     print("\n\tError: {:s}".format(e))
-
-    #---------------------------------------#
+    # ------------------#
     print("\n\tConverting data into PhysicalTensor ... ", end="")
     dipoles = PhysicalTensor(dipoles)
     print("done")
@@ -84,6 +77,6 @@ def main(args):
     print("done")
 
 
-#---------------------------------------#
+# ---------------------------------------#
 if __name__ == "__main__":
     main()
