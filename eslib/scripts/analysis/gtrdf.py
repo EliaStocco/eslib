cf6af3a6a4ec4bf091328aac7978408cf82facbd
EliaStocco <eliastocco@gmail.com>
2024-10-25 16:46:56 +0200

    updated script

--- eslib/scripts/analysis/gtrdf.py
+++ eslib/scripts/analysis/gtrdf.py
@@ -8,11 +8,11 @@
 @Desc    :   None
 '''
 
-
 from __future__ import absolute_import, division, print_function
 
-import sys
-
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('QtAgg')
 import numpy as np
 
 from eslib.classes.atomic_structures import AtomicStructures
@@ -21,36 +21,7 @@ from eslib.fortran import fortran_rdfs
 from eslib.input import itype, size_type
 from eslib.physics import get_element_mass
 
-description = "Calculate the radial distribution functions for salty water"
-
-def index_in_slice(slice_obj: slice, index: int) -> bool:
-    """
-    Check if the given index is within the specified slice object.
-
-    Parameters:
-    slice_obj (slice): The slice object to check against.
-    index (int): The index to check.
-
-    Returns:
-    bool: True if the index is within the slice, False otherwise.
-    """
-    # Create a range object from the slice object's start, stop, and step attributes
-    range_obj = range(slice_obj.start if slice_obj.start else 0,
-                      slice_obj.stop if slice_obj.stop else sys.maxsize,
-                      slice_obj.step if slice_obj.step else 1)
-
-    # Check if the index is within the range object
-    return index in range_obj
-
-def string_to_slice(s: str) -> slice:
-    if s == ":":
-        slc = slice(None)
-    else:
-        try:
-            slc = int(s)
-        except ValueError:
-            slc = slice(*[int(s) if s else None for s in s.split(":")])
-    return slc
+description = "Calculate the Radial Distribution Function (RDF) using a Fortran routine."
 
 def prepare_args(description):
     import argparse
@@ -62,19 +33,10 @@ def prepare_args(description):
     parser.add_argument("-in", "--index"       , **argv, required=False, type=itype, help="index to be read from input file (default: %(default)s)", default=':')
     parser.add_argument("-e" , "--elements"    , **argv, required=True , type=el   , help="elements list")
     parser.add_argument("-n" , "--nbins"       , **argv, required=False, type=int  , help="number of bins to divide RDF (default: %(default)s)", default=100)
-    parser.add_argument("-m" , "--min"         , **argv, required=False, type=float, help="minimum distance in the plot (default: %(default)s)", default=1.)
     parser.add_argument("-r1", "--rmin"        , **argv, required=False, type=float, help="minimum distance of RDF (default: %(default)s)", default=1.)
     parser.add_argument("-r2", "--rmax"        , **argv, required=False, type=float, help="maximum distance of RDF (default: %(default)s)", default=5.)
     parser.add_argument("-o" , "--output"      , **argv, required=False, type=str  , help="output file (default: %(default)s)", default="rdf.csv")
-    
-    # parser.add_argument("--index", default=':', help="frames to include in the calculation")
-    # parser.add_argument("--rmin", type=float, default=0, help="Minimum distance for RDF calculation in Angstrom")
-    # parser.add_argument("--rmax", type=float, default=6, help="Maximum distance for RDF calculation in Angstrom")
-    # parser.add_argument("--bins", type=int, default=100, help="Number of bins")
-    # parser.add_argument("--stride", type=int, default=1, help="Stride with which to print the RDF data to file.")
-    # parser.add_argument("a1", help="Chemical symbol for the first atom in the RDF pair")
-    # parser.add_argument("a2", help="Chemical symbol for the second atom in the RDF pair")
-    # parser.add_argument("traj", nargs='+', help="XYZ trajectory files produced by i-PI")
+    parser.add_argument("-p" , "--plot"        , **argv, required=False, type=str  , help="plot file (default: %(default)s)", default=None)
     return parser 
 #---------------------------------------#
 @esfmt(prepare_args, description)
@@ -101,7 +63,7 @@ def main(args):
     )
     shellVolumes = 4*np.pi/3 * ((rdf[:, 0] + 0.5 * dr) ** 3 - (rdf[:, 0] - 0.5 * dr) ** 3)
     
-    mass = trajectory[0].get_masses()
+    # mass = trajectory[0].get_masses()
     
     massA, massB = get_element_mass(args.elements) # Elements.mass(args.elements[0])
     
@@ -117,8 +79,8 @@ def main(args):
         ]
     natomsA = len(species_A)
     natomsB = len(species_B)
-    print("\tindices for {:s}: {} (tot. {:d})".format(args.elements[0], species_A, natomsA))
-    print("\tindices for {:s}: {} (tot. {:d})".format(args.elements[1], species_B, natomsB))
+    print("\tn. of {:s} atoms: {:d}".format(args.elements[0], natomsA))
+    print("\tn. of {:s} atoms: {:d}".format(args.elements[1], natomsB))
     
     posA = np.zeros(natomsA, order="F")
     posB = np.zeros(natomsB, order="F")
@@ -144,6 +106,24 @@ def main(args):
     header = "r [ang], RDF"
     np.savetxt(args.output, rdf,fmt=float_format,header=header)
     print("done")
+    
+    if args.plot is not None:
+        print("\tPreparing plot ... ".format(args.plot), end="")
+        plt.figure(figsize=(6,4))
+        plt.plot(rdf[:,0],rdf[:,1],color="blue")
+        plt.xlabel("distance $[\\AA]$")
+        plt.ylabel("RDF")
+        plt.xlim(args.rmin,args.rmax)
+        plt.ylim(0,None)
+        plt.grid()
+        # plt.legend()
+        plt.tight_layout()        
+        print("done\n")
+
+        print("\tSaving plot to file '{:s}' ... ".format(args.plot), end="")
+        plt.savefig(args.plot)
+        plt.close()
+        print("done\n")
     
     return 0 
 
