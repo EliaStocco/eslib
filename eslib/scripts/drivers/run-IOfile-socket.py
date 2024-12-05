4876880dff4ee5ca8c9eaa488442fc07db4f1cd9
EliaStocco <eliastocco@gmail.com>
2024-12-05 22:43:11 +0100

    script to run FileIOCalculator

---
+++ eslib/scripts/drivers/run-IOfile-socket.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from ase.io import read
+from ase.calculators.socketio import SocketClient
+from eslib.drivers.socketextras import SocketClientExtras
+from eslib.formatting import esfmt
+from eslib.input import str2bool
+from eslib.classes.calculators.fileIOcalculator import FileIOCalculator
+
+#---------------------------------------#
+description = "Run a FileIOCalculator."
+
+#---------------------------------------#
+def prepare_args(description):
+    import argparse
+    parser = argparse.ArgumentParser(description=description)
+    argv = {"metavar" : "\b",}
+    parser.add_argument("-i" , "--input"        , **argv, required=True , type=str     , help="file with the atomic structure")
+    parser.add_argument("-if", "--input_format" , **argv, required=False, type=str     , help="file format of the atomic structure (default: %(default)s)" , default=None)
+    parser.add_argument("-p" , "--port"         , **argv, required=False, type=int     , help="TCP/IP port number. Ignored when using UNIX domain sockets.")
+    parser.add_argument("-a" , "--address"      , **argv, required=True , type=str     , help="Host name (for INET sockets) or name of the UNIX domain socket to connect to.")
+    parser.add_argument("-u" , "--unix"         , **argv, required=False, type=str2bool, help="Use a UNIX domain socket (default: %(default)s)", default=False)
+    parser.add_argument("-sc", "--socket_client", **argv, required=False, type=str     , help="socket client (default: %(default)s)", choices=['eslib','ase'], default='ase')
+    parser.add_argument("-f" , "--folder"       , **argv, required=True , type=str     , help="folder where the files will be written and read")
+    return parser
+
+#---------------------------------------#
+@esfmt(prepare_args,description)
+def main(args):
+    
+    #------------------#
+    print("\tAllocating the calculator ... ", end="")
+    calculator = FileIOCalculator(args.folder)
+    print("done")
+    
+    #------------------#
+    print("\tReading the first atomic structure from file '{:s}' ... ".format(args.input), end="")
+    atoms = read(args.input,format=args.input_format,index=0)
+    atoms.info = {} # I don't know if I actually need this line
+    print("done")
+
+    atoms.calc = calculator
+
+    #------------------#
+    socket_client = SocketClientExtras if args.socket_client == 'eslib' else SocketClient
+    print("\tPreparing the socket communication ... ", end="")
+    client = socket_client(host=args.address,\
+                          port=args.port,\
+                          unixsocket=args.address if args.unix else None)
+    print("done")
+
+    #------------------#
+    print("\n\tRunning ... ", end="")
+    client.run(atoms,use_stress=False)
+    print("done")
+
+#---------------------------------------#
+if __name__ == "__main__":
+    main()
