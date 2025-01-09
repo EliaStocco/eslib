a5c12f6996de1c07cc7c70e27994517b6934b1a8
EliaStocco <eliastocco@gmail.com>
2025-01-09 10:11:58 +0100

    small changes

--- eslib/scripts/inspect/mean_std_err.py
+++ eslib/scripts/inspect/mean_std_err.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from eslib.formatting import esfmt
 from eslib.mathematics import mean_std_err2pandas
 from eslib.plot import legend_options
+from eslib.input import str2bool
 
 #---------------------------------------#
 description = "Computes the mean, standard deviation and standard error of some data + produce a plot."
@@ -13,11 +14,12 @@ def prepare_args(description):
     import argparse
     parser = argparse.ArgumentParser(description=description)
     argv = {"metavar" : "\b",}
-    parser.add_argument("-i" , "--input"    , **argv, required=True , type=str  , help="input files")
-    parser.add_argument("-a" , "--axis"     , **argv, required=False, type=int  , help='axis (default: %(default)s)', default=1)
-    parser.add_argument("-o" , "--output"   , **argv, required=True , type=str  , help="output file")
-    parser.add_argument("-p" , "--plot"     , **argv, required=None , type=str  , help="plot file (default: %(default)s)", default=None)
-    parser.add_argument("-dt", "--time_step", **argv, required=False, type=float, help="time step (default: %(default)s)", default=1)
+    parser.add_argument("-i" , "--input"    , **argv, required=True , type=str     , help="input files")
+    parser.add_argument("-a" , "--axis"     , **argv, required=False, type=int     , help='axis (default: %(default)s)', default=1, choices=[0,1])
+    parser.add_argument("-d" , "--delta"    , **argv, required=False, type=str2bool, help='delta w.r.t. first value (default: %(default)s)', default=False)
+    parser.add_argument("-o" , "--output"   , **argv, required=True , type=str     , help="output file")
+    parser.add_argument("-p" , "--plot"     , **argv, required=None , type=str     , help="plot file (default: %(default)s)", default=None)
+    parser.add_argument("-dt", "--time_step", **argv, required=False, type=float   , help="time step (default: %(default)s)", default=1)
     return parser
     
 #---------------------------------------#
@@ -30,6 +32,17 @@ def main(args):
     print("done")
     print("\tshape: ",data.shape)
     assert data.ndim == 2, "The input file must be a 2D array"
+    assert args.axis < 2, "Axis must be 0 or 1"
+    
+    #------------------#
+    if args.delta:
+        print("\n\tComputing the delta ... ", end="")
+        other = 1 if args.axis == 0 else 0
+        value = np.take(data,0, axis=other)
+        value = value[:,np.newaxis] if args.axis == 0 else value[np.newaxis,:]
+        data = data - value
+        print("done")
+        
     
     #------------------#
     print("\n\tComputing the mean, standard deviation and standard error ... ", end="")
@@ -46,10 +59,11 @@ def main(args):
         print("\n\tSaving plot to file '{:s}' ... ".format(args.plot), end="")
         fig,ax = plt.subplots(figsize=(4,3))
         time = np.arange(len(df))*args.time_step
-        ax.plot(time,df["mean"],label="mean")
-        ax.fill_between(time,df["mean"]-df["err"],df["mean"]+df["err"],alpha=0.6)
-        ax.fill_between(time,df["mean"]-df["std"],df["mean"]+df["std"],alpha=0.3)
+        ax.fill_between(time,df["mean"]-df["std"],df["mean"]+df["std"],alpha=0.3,color="green",label="std",linewidth=0)
+        ax.fill_between(time,df["mean"]-df["err"],df["mean"]+df["err"],alpha=0.6,color="red",label="err",linewidth=0)
+        ax.plot(time,df["mean"],label="mean",color="blue",alpha=1,linewidth=0.5)
         ax.legend(**legend_options)
+        plt.grid()
         plt.tight_layout()                
         plt.savefig(args.plot,bbox_inches="tight",dpi=300)  
         print("done")
