bdea578898c07a6785f118f57b90f4375e4a0fd5
EliaStocco <eliastocco@gmail.com>
2025-10-30 11:24:03 +0100

    not so useful anymore

--- eslib/scripts/cube/hartree2polarization.py
+++ eslib/scripts/cube/hartree2polarization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy as np
 from ase.io.cube import read_cube_data
+from sympy import E
 from eslib.classes.atomic_structures import AtomicStructures
 from eslib.formatting import esfmt, eslog
 from eslib.tools import convert
@@ -23,7 +24,7 @@ def prepare_args(description):
 def main(args):
 
     #------------------#
-    # trajectory
+    # read Hartree potential from a cube file
     print("\tReading the first atomic structure from file '{:s}' ... ".format(args.input), end="")
     atoms = AtomicStructures.from_file(file=args.input,index=0)[0]
     cube = CubeData.from_file(args.input)
@@ -31,21 +32,25 @@ def main(args):
     assert np.allclose(atoms.cell.T,cube.vectors), "Cell and cube vectors differ."
     
     #------------------#
+    # summary of the Hartree potential
     s = cube.summary(title="Hartee potential",prefix="\t"); print(s)
     
     #------------------#
-    k = cube.kspace()
-    r = k.rspace()
-    assert r == cube, "codin error"
-    
-    
-    #------------------#
+    # compute the Electric field (gradient) using spatial Fourier transform
     print("\tComputing the electric field ... ",end="")
     Efield = cube.gradient()
     print("done")
-    Efield.summary(title="Electric field")       
-    return
+    # summary of the Electric field
+    s = Efield.summary(title="Electric field",prefix="\t"); print(s)       
     
+    #------------------#
+    # integrate the electric field to get the polarization
+    print("\tComputing polarization ... ",end="")
+    polarization = Efield.integrate()
+    print("done")
+    print("\tpolarization: ",polarization.tolist())
+    
+    return
     
 #---------------------------------------#
 if __name__ == "__main__":
